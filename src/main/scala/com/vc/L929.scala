fe4db2f4886f794f1de05da3af14e392f71459c3
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-10-31 23:58:32 -0700

    L929

---
+++ src/main/scala/com/vc/L929.scala
@@ -0,0 +1,19 @@
+package com.vc
+
+object L929 {
+  def numUniqueEmails(emails: Array[String]): Int = {
+    import scala.collection.mutable
+    val set = new mutable.HashSet[String]()
+
+    emails.foreach(email => {
+      set += cleanUpEmail(email)
+    })
+    set.size
+  }
+
+  def cleanUpEmail(email: String): String = {
+    val splitByAt = email.split('@')
+    val splitByPlus = splitByAt(0).split('+')
+    splitByPlus(0).replaceAll("\\.", "") + "@" + splitByAt(1)
+  }
+}
