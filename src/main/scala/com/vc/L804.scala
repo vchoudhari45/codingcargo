53d5eb585bcddd58a5d6cb4864483d4672a62828
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-01 00:41:45 -0700

    L804

---
+++ src/main/scala/com/vc/L804.scala
@@ -0,0 +1,20 @@
+package com.vc
+
+object L804 {
+  def uniqueMorseRepresentations(words: Array[String]): Int = {
+    val arr = Array(".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..")
+
+    import scala.collection.mutable
+    val set = new mutable.HashSet[String]()
+    val str = new mutable.StringBuilder()
+    words.foreach(word => {
+      word.foreach(ch => {
+        val idx = ch - 'a'
+        str.append(arr(idx))
+      })
+      set += str.toString
+      str.clear
+    })
+    set.size
+  }
+}
