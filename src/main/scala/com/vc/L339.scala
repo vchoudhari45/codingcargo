45cf22ee31e6d2589e1b808f7588193a88060df2
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-02 01:14:10 -0700

    L339

---
+++ src/main/scala/com/vc/L339.scala
@@ -0,0 +1,32 @@
+package com.vc
+
+
+class NestedInteger {
+  // Return true if this NestedInteger holds a single integer, rather than a nested list.
+  def isInteger: Boolean = ???
+  // Return the single integer that this NestedInteger holds, if it holds a single integer
+  def getInteger: Int = ???
+  // Set this NestedInteger to hold a single integer.
+  def setInteger(i: Int) = ???
+  // Return the nested list that this NestedInteger holds, if it holds a nested list
+  def getList: List[NestedInteger] = ???
+  // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+  def add(ni: NestedInteger) = ???
+}
+
+object Solution {
+  //[[1,1],2,[1,1]]
+  def depthSum(nestedList: List[NestedInteger]): Int = {
+    var sum = 0
+    nestedList.foreach(x => depth(x, 1))
+    def depth(nestedInteger: NestedInteger, d:Int): Unit = {
+      if(nestedInteger.isInteger){
+        sum += nestedInteger.getInteger * d
+      }
+      else {
+        nestedInteger.getList.foreach(x => depth(x, d + 1))
+      }
+    }
+    sum
+  }
+}
