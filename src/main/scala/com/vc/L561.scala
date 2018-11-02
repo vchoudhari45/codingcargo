b1edca6e1de66fd46f43f4e846b539278a7d438a
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-01 23:13:41 -0700

    L561

---
+++ src/main/scala/com/vc/L561.scala
@@ -0,0 +1,12 @@
+package com.vc
+
+object L561 {
+  def arrayPairSum(nums: Array[Int]): Int = {
+    scala.util.Sorting.quickSort(nums)
+    var sum = 0
+    (nums.indices by 2).foreach(i => {
+      sum += nums(i)
+    })
+    sum
+  }
+}
