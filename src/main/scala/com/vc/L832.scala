8c0bb4dd038230088bc39f5ad775a6cd94700986
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-01 22:15:54 -0700

    L832

---
+++ src/main/scala/com/vc/L832.scala
@@ -0,0 +1,16 @@
+package com.vc
+
+object L832 {
+  def flipAndInvertImage(arr: Array[Array[Int]]): Array[Array[Int]] = {
+    arr.indices.foreach(i => {
+      arr(i) = arr(i).reverse
+    })
+
+    arr.indices.foreach(i => {
+      arr(i).indices.foreach(j => {
+        arr(i)(j) = arr(i)(j) ^ 1
+      })
+    })
+    arr
+  }
+}
