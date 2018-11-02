f0470ef723f4f31c769831cffbe199b31ad6773f
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-01 23:52:43 -0700

    L728

---
+++ src/main/scala/com/vc/L728.scala
@@ -0,0 +1,23 @@
+package com.vc
+
+import scala.annotation.tailrec
+
+object L728 {
+  def selfDividingNumbers(left: Int, right: Int): List[Int] = {
+    import scala.collection.mutable
+    val res = new mutable.ListBuffer[Int]()
+    (left to right).foreach(i => {
+      if(isSelfDividingNumber(i, i)) res += i
+    })
+    res.toList
+  }
+
+  @tailrec def isSelfDividingNumber(original: Int, number: Int): Boolean = {
+    if(original < 10) return true
+    val digit = number % 10
+    if(digit == 0) return false
+    if(digit != 1 && original % digit != 0) return false
+    if(number < 10) return true
+    isSelfDividingNumber(original, number / 10)
+  }
+}
