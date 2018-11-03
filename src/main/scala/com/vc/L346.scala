f5ca754ba12ff6d1e8254f9210e154db17014665
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-02 20:52:16 -0700

    L346

---
+++ src/main/scala/com/vc/L346.scala
@@ -0,0 +1,23 @@
+package com.vc
+
+class MovingAverage(size: Int) {
+
+  /** Initialize your data structure here. */
+  import scala.collection.mutable
+  val q = new mutable.Queue[Int]()
+
+  def next(value: Int): Double = {
+    if(q.size == size) {
+      q.dequeue()
+    }
+    q.enqueue(value)
+    q.sum.toDouble / q.size.toDouble
+  }
+
+}
+
+/**
+  * Your MovingAverage object will be instantiated and called as such:
+  * var obj = new MovingAverage(size)
+  * var param_1 = obj.next(`val`)
+  */
