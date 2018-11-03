ca1cd89b10544ad76923a0d9e3953f464ada2370
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-03 14:15:05 -0700

    L876

---
+++ src/main/scala/com/vc/L876.scala
@@ -0,0 +1,18 @@
+package com.vc
+
+class ListNode(var _x: Int = 0) {
+  var next: ListNode = null
+  var x: Int = _x
+}
+
+object Solution {
+  def middleNode(head: ListNode): ListNode = {
+    var fast = head
+    var slow = head
+    while(fast != null && fast.next != null) {
+      slow = slow.next
+      fast = fast.next.next
+    }
+    slow
+  }
+}
