7745bbe1434b8855886b56bbeeee09551d05676f
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-03 16:14:54 -0700

    L557

---
+++ src/main/scala/com/vc/L557.scala
@@ -0,0 +1,29 @@
+package com.vc
+
+object L557 {
+  def reverseWords(s: String): String = {
+    val sArr = s.toCharArray
+    var i = 0
+    var j = 0
+    val n = s.length
+    while(i < n) {
+      j = i
+      while(j < n && sArr(j) != ' ') j += 1
+      reverse(i, j - 1)
+      i = j + 1
+    }
+
+    def reverse(s:Int, d: Int): Unit = {
+      var source = s
+      var dest = d
+      while(source < dest) {
+        val tmp = sArr(source)
+        sArr(source) = sArr(dest)
+        sArr(dest) = tmp
+        source += 1
+        dest -= 1
+      }
+    }
+    sArr.mkString("")
+  }
+}
