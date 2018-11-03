ada9fac78cdb862810be7b98a8b5bb168fc9684a
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-03 15:04:25 -0700

    L559

---
+++ src/main/scala/com/vc/L559.java
@@ -0,0 +1,29 @@
+package com.vc;
+
+
+import java.util.List;
+
+class Node559 {
+    public int val;
+    public List<Node559> children;
+
+    public Node559() {}
+
+    public Node559(int _val, List<Node559> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
+class L559 {
+    public int maxDepth(Node559 root) {
+        int maxValue = 0;
+        if(root != null) {
+            for(Node559 node: root.children) {
+                maxValue = Math.max(maxValue, maxDepth(node));
+            }
+            return 1 + maxValue;
+        }
+        return maxValue;
+    }
+}
--- src/main/scala/com/vc/L589.java
+++ src/main/scala/com/vc/L589.java
@@ -3,29 +3,29 @@ package com.vc;
 import java.util.ArrayList;
 import java.util.List;
 
-class Node {
+class Node589 {
     public int val;
-    public List<Node> children;
+    public List<Node589> children;
 
-    public Node() {}
+    public Node589() {}
 
-    public Node(int _val,List<Node> _children) {
+    public Node589(int _val,List<Node589> _children) {
         val = _val;
         children = _children;
     }
 }
 
 class L589 {
-    public List<Integer> preorder(Node root) {
+    public List<Integer> preorder(Node589 root) {
         List<Integer> list = new ArrayList<Integer>();
         preOrder(root, list);
         return list;
     }
 
-    private void preOrder(Node root, List<Integer> list) {
+    private void preOrder(Node589 root, List<Integer> list) {
         if(root != null) {
             list.add(root.val);
-            for(Node node: root.children) {
+            for(Node589 node: root.children) {
                 preOrder(node, list);
             }
         }
