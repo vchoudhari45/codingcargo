f3775b32f27d979881038705052643c3b9167a70
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-03 15:31:41 -0700

    L806

---
+++ src/main/scala/com/vc/L806.scala
@@ -0,0 +1,19 @@
+package com.vc
+
+object L806 {
+  def numberOfLines(widths: Array[Int], s: String): Array[Int] = {
+    var lines = 1
+    var current = 0
+    s.foreach(ch => {
+      val width = widths(ch - 'a')
+      if(current + width > 100) {
+        lines += 1
+        current = width
+      }
+      else {
+        current = current + width
+      }
+    })
+    Array(lines, current)
+  }
+}
