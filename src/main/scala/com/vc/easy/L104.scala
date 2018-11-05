8332dd5d2d185edb570ee738658b78d1dda21c9e
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-04 22:28:57 -0800

    L104

--- src/main/scala/com/vc/easy/L104.scala
+++ src/main/scala/com/vc/easy/L104.scala
@@ -8,7 +8,25 @@ class TreeNode104(var _value: Int) {
 
 object Solution {
   def maxDepth(root: TreeNode104): Int = {
+    /**
+      * if(root == null) return 0
+      * 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+      **/
     if(root == null) return 0
-    1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+    import scala.collection.mutable
+    val q = new mutable.Queue[TreeNode104]()
+    q.enqueue(root)
+    var res = 0
+    while(q.nonEmpty) {
+      var size = q.size
+      while(size != 0) {
+        val e = q.dequeue()
+        if(e.left != null) q.enqueue(e.left)
+        if(e.right != null) q.enqueue(e.right)
+        size -= 1
+      }
+      res += 1
+    }
+    res
   }
 }
