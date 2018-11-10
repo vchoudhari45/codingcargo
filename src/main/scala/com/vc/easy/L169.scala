74aeccf03e04d451274a5011d4617587fc2025aa
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-10 15:50:16 -0800

    L171

--- src/main/scala/com/vc/easy/L169.scala
+++ src/main/scala/com/vc/easy/L169.scala
@@ -1,5 +1,20 @@
 package com.vc.easy
 
-class L169 {
-
+object L169 {
+  def majorityElement(num: Array[Int]): Int = {
+    var i = 1
+    var res = num(0)
+    var count = 1
+    val n = num.length
+    while(i < n) {
+      if(count == 0) {
+        res = num(i)
+        count = 1
+      }
+      else if(res == num(i)) count += 1
+      else count -= 1
+      i += 1
+    }
+    res
+  }
 }
