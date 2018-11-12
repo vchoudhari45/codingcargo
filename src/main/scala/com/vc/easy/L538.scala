51593f4730a63ea3f6a7c899008c01a8c1621865
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-11 23:53:08 -0800

    L538

--- src/main/scala/com/vc/easy/L538.scala
+++ src/main/scala/com/vc/easy/L538.scala
@@ -8,6 +8,7 @@ class TreeNode538(var _value: Int) {
 
 object Solution {
   def convertBST(root: TreeNode538): TreeNode538 = {
+    /**
     var sum = 0
     solve(root)
     def solve(root: TreeNode538): Unit = {
@@ -17,6 +18,24 @@ object Solution {
         root.value += sum
         sum += right
         solve(root.left)
+      }
+    }
+    root*/
+    var sum = 0
+    import scala.collection.mutable
+    val st = new mutable.Stack[TreeNode538]()
+    var current = root
+    while(current != null || st.nonEmpty) {
+      if(current != null) {
+        st.push(current)
+        current = current.right
+      }
+      else {
+        current = st.pop
+        val oldValue = current.value
+        current.value += sum
+        sum += oldValue
+        current = current.left
       }
     }
     root
