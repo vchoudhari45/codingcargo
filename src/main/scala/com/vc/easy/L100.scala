a4cae2e28ea2ae20852e51cbdf8ad600e017a187
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-12 00:11:21 -0800

    L100

---
+++ src/main/scala/com/vc/easy/L100.scala
@@ -0,0 +1,32 @@
+package com.vc.easy
+
+
+class TreeNode100(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode100 = null
+  var right: TreeNode100 = null
+}
+
+object Solution {
+  def isSameTree(p: TreeNode100, q: TreeNode100): Boolean = {
+    //(p ==null && q == null) || (p != null && q != null && p.value == q.value && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
+    import scala.collection.mutable
+    val st = new mutable.Stack[TreeNode100]()
+    st.push(p)
+    st.push(q)
+    while(st.nonEmpty) {
+      val qe = st.pop
+      val pe = st.pop
+      if((pe == null && qe == null) || (pe != null && qe != null && pe.value == qe.value)) {
+        if(pe != null && qe != null) {
+          st.push(pe.right)
+          st.push(qe.right)
+          st.push(pe.left)
+          st.push(qe.left)
+        }
+      }
+      else return false
+    }
+    true
+  }
+}
