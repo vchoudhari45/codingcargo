aa384ef06f48b78899e001ebf2e9fa466b5c3d38
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-12 01:34:32 -0800

    L783

---
+++ src/main/scala/com/vc/easy/L783.scala
@@ -0,0 +1,32 @@
+package com.vc.easy
+
+class TreeNode783(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode783 = null
+  var right: TreeNode783 = null
+}
+
+object Solution {
+  def minDiffInBST(root: TreeNode783): Int = {
+    import scala.collection.mutable
+    val st = new mutable.Stack[TreeNode783]()
+    var current = root
+    var res = Int.MaxValue
+    var prev = -1
+    while(current != null || st.nonEmpty) {
+      if(current != null) {
+        st.push(current)
+        current = current.right
+      }
+      else {
+        current = st.pop
+        if(prev != -1) {
+          res = Math.min(res, prev - current.value)
+        }
+        prev = current.value
+        current = current.left
+      }
+    }
+    res
+  }
+}
