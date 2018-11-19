78f01b6214f4d6305a1e738eb1cf9fb7d3b6d4d6
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-19 00:27:11 -0800

    TreeTraversal

---
+++ src/main/scala/com/vc/easy/TreeTraversal.scala
@@ -0,0 +1,80 @@
+package com.vc.easy
+
+class TreeNode(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode = null
+  var right: TreeNode = null
+}
+
+/**
+  *     1
+  *   2   3
+  *     4
+  **/
+object TreeTraversal {
+  import scala.collection.mutable
+  def main(args: Array[String]): Unit = {
+    val root = new TreeNode(1)
+    val left = new TreeNode(2)
+    left.right = new TreeNode(4)
+    root.left = left
+    root.right = new TreeNode(3)
+
+    inOrder(root)
+    println("=========")
+    preOrder(root)
+    println("=========")
+    postOrder(root)
+    println("=========")
+  }
+
+  def inOrder(root: TreeNode): Unit = {
+    val st = new mutable.Stack[TreeNode]()
+    var current = root
+    while(current != null || st.nonEmpty) {
+      if(current != null) {
+        st.push(current)
+        current = current.left
+      }
+      else {
+        current = st.pop
+        println(current.value)
+        current = current.right
+      }
+    }
+  }
+
+  def preOrder(root: TreeNode): Unit = {
+    val st = new mutable.Stack[TreeNode]()
+    var current = root
+    while(current != null || st.nonEmpty) {
+      if(current != null) {
+        println(current.value)
+        st.push(current)
+        current = current.left
+      }
+      else current = st.pop.right
+    }
+  }
+
+  def postOrder(root: TreeNode): Unit = {
+    val st = new mutable.Stack[TreeNode]()
+    var current = root
+    while(current != null || st.nonEmpty) {
+      if(current != null) {
+        st.push(current)
+        current = current.left
+      }
+      else {
+        var tmp = st.head.right
+        if(tmp == null) {
+          do {
+            tmp = st.pop
+            println(tmp.value)
+          } while(st.nonEmpty && tmp == st.head.right)
+        }
+        else current = tmp
+      }
+    }
+  }
+}
