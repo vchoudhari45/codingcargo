a53dcf241843b0fa543cbd5812bf0f913999968d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-24 01:33:39 -0800

    L191

---
+++ src/main/scala/com/vc/easy/L191.java
@@ -0,0 +1,13 @@
+package com.vc.easy;
+
+public class L191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ones = 0;
+        while(n != 0) {
+            ones = ones + (n & 1);
+            n = n >>> 1;
+        }
+        return ones;
+    }
+}
