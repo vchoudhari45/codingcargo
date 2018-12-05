9210f0af9864fd816ba0823dd514ac6981d4e71c
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-12-05 01:24:19 -0800

    L112

---
+++ src/main/scala/com/vc/easy/L112.scala
@@ -0,0 +1,15 @@
+package com.vc.easy
+
+class TreeNode112(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode112 = null
+  var right: TreeNode112 = null
+}
+
+object L112 {
+  def hasPathSum(root: TreeNode112, sum: Int): Boolean = {
+    if(root == null) return false
+    else if(root.left == null && root.right == null) return root.value == sum
+    else hasPathSum(root.left, sum - root.value) || hasPathSum(root.right, sum -root.value)
+  }
+}
