0afbf654ba87a752336bee43aa0cdfefef327ca0
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-01-24 21:08:45 -0800

    L877

---
+++ src/main/scala/com/vc/medium/L406.scala
@@ -0,0 +1,7 @@
+package com.vc.medium
+
+object L406 {
+  def reconstructQueue(people: Array[Array[Int]]): Array[Array[Int]] = {
+
+  }
+}
--- src/main/scala/com/vc/medium/L877.scala
+++ src/main/scala/com/vc/medium/L877.scala
@@ -1,7 +1,15 @@
 package com.vc.medium
 
 object L877 {
-  def stoneGame(piles: Array[Int]): Boolean = {
-
+  def stoneGame(arr: Array[Int]): Boolean = {
+    val n = arr.length
+    val dp = new Array[Int](n)
+    dp.indices.foreach(i => dp(i) = arr(i))
+    (1 until n).foreach(d => {
+      (0 until n - d).foreach(i => {
+        dp(i) = Math.max(arr(i) - dp(i), arr(i + d) - dp(i + d - 1))
+      })
+    })
+    dp(0) > 0
   }
 }
