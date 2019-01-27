28d31c666bf9cda1d4ce2a604628f5562a116104
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-01-27 13:48:34 -0800

    L406

--- src/main/scala/com/vc/medium/L406.scala
+++ src/main/scala/com/vc/medium/L406.scala
@@ -1,7 +1,26 @@
 package com.vc.medium
 
-object L406 {
-  def reconstructQueue(people: Array[Array[Int]]): Array[Array[Int]] = {
 
+object L406 {
+  object PeopleSorting extends Ordering[Array[Int]] {
+    def compare(x: Array[Int], y: Array[Int]): Int = {
+      val cmp = y(0).compareTo(x(0))
+      if(cmp == 0) x(1).compareTo(y(1)) else cmp
+    }
+  }
+  def reconstructQueue(people: Array[Array[Int]]): Array[Array[Int]] = {
+    scala.util.Sorting.quickSort(people)(PeopleSorting)
+    people.indices.foreach(i => {
+      val desiredIndex = people(i)(1)
+      move(desiredIndex, i)
+    })
+    def move(desiredIndex: Int, currentIndex: Int): Unit = {
+      val e = people(currentIndex)
+      (currentIndex until desiredIndex by - 1).foreach(i => {
+        people(i) = people(i - 1)
+      })
+      people(desiredIndex) = e
+    }
+    people
   }
 }
