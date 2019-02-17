0d6d55a87965a63255b673e1cda9a7c6a8641dad
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-02-17 15:45:34 -0800

    L817

---
+++ src/main/scala/com/vc/medium/L817.scala
@@ -0,0 +1,22 @@
+package com.vc.medium
+
+
+class ListNode817(var _x: Int = 0) {
+  var next: ListNode817 = null
+  var x: Int = _x
+}
+
+object Solution {
+  def numComponents(head: ListNode817, arr: Array[Int]): Int = {
+    import scala.collection.mutable
+    val set = new mutable.HashSet[Int]()
+    arr.foreach(x => set += x)
+    var current = head
+    var res = 0
+    while(current != null) {
+      if(set.contains(current.x) && (current.next == null || !set.contains(current.next.x))) res += 1
+      current = current.next
+    }
+    res
+  }
+}
