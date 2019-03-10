fdf3d57a83b411aac1c6c4e375d7875914d5efa4
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-03-09 17:26:33 -0800

    L451

---
+++ src/main/scala/com/vc/medium/L451.scala
@@ -0,0 +1,27 @@
+package com.vc.medium
+
+object PQOrdering extends Ordering[(Char, Int)] {
+  def compare(x: (Char, Int), y: (Char, Int)): Int = x._2.compareTo(y._2)
+}
+class L451 {
+  def frequencySort(s: String): String = {
+    import scala.collection.mutable
+    val map = new mutable.HashMap[Char, Int]()
+    s.foreach(x => {
+      val count = map.getOrElse(x, 0)
+      map.put(x, count + 1)
+    })
+
+    val pq = new mutable.PriorityQueue[(Char, Int)]()(PQOrdering)
+    map.foreach(x => {
+      pq.enqueue(x)
+    })
+
+    val str = new mutable.StringBuilder()
+    while(pq.nonEmpty) {
+      val e = pq.dequeue()
+      (0 until e._2).foreach(_ => str.append(e._1))
+    }
+    str.toString
+  }
+}
