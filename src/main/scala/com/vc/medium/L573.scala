f51ad320db5a9d2d3154e38cd92ac194098f2429
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-03-26 20:21:23 -0700

    L573

--- src/main/scala/com/vc/medium/L573.scala
+++ src/main/scala/com/vc/medium/L573.scala
@@ -3,7 +3,7 @@ package com.vc.medium
 object L573 {
   def minDistance(height: Int, width: Int, tree: Array[Int], squirrel: Array[Int], nuts: Array[Array[Int]]): Int = {
     var sum = 0
-    var maxDiff = 0
+    var maxDiff = Int.MinValue
     nuts.foreach(nut => {
       val distNutToTree = Math.abs(nut(0) - tree(0)) + Math.abs(nut(1) - tree(1))
       sum += 2 * distNutToTree
