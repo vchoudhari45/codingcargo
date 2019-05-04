759e92b3ffa53fba49e665470130d6cc5a15e39d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-05-04 15:42:49 -0700

    L230

--- src/main/scala/com/vc/medium/L230.scala
+++ src/main/scala/com/vc/medium/L230.scala
@@ -8,5 +8,19 @@ class TreeNode230(var _value: Int) {
 }
 
 object L230 {
-  def kthSmallest(root: TreeNode230, k: Int): Int = ???
+  def kthSmallest(root: TreeNode230, k: Int): Int = {
+    var count = k
+    var res = 0
+    def solve(root: TreeNode230): Unit = {
+      if(root.left != null) solve(root.left)
+      count -= 1
+      if(count == 0) {
+        res = root.value
+        return
+      }
+      if(root.right != null) solve(root.right)
+    }
+    solve(root)
+    res
+  }
 }
