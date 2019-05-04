4bebda75b5527b0d54f40eb1277df404b2130504
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-05-04 15:24:07 -0700

    L144

---
+++ src/main/scala/com/vc/medium/L144.scala
@@ -0,0 +1,24 @@
+package com.vc.medium
+
+
+class TreeNodeL144(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNodeL144 = null
+  var right: TreeNodeL144 = null
+}
+
+object Solution {
+  def preorderTraversal(root: TreeNodeL144): List[Int] = {
+    import scala.collection.mutable
+    val list = new mutable.ListBuffer[Int]()
+    def solve(root: TreeNodeL144): Unit = {
+      if(root != null) {
+        list += root.value
+        solve(root.left)
+        solve(root.right)
+      }
+    }
+    solve(root)
+    list.toList
+  }
+}
