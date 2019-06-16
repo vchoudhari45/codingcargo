fb7fd14601204346146673f941d22c5e844255f3
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-06-16 01:23:22 -0700

    L360

--- src/main/scala/com/vc/medium/L360.scala
+++ src/main/scala/com/vc/medium/L360.scala
@@ -1,5 +1,39 @@
 package com.vc.medium
 
 object L360 {
-  def sortTransformedArray(nums: Array[Int], a: Int, b: Int, c: Int): Array[Int] = ???
+  def sortTransformedArray(arr: Array[Int], a: Int, b: Int, c: Int): Array[Int] = {
+    def solve(x: Int): Int = a * Math.pow(x, 2).toInt + b * x + c
+    val n = arr.length
+    val sorted = new Array[Int](n)
+    var i = 0
+    var j = n - 1
+    var index = if(a >= 0) n - 1 else 0
+    while(i <= j) {
+      val iIndexValue = solve(arr(i))
+      val jIndexValue = solve(arr(j))
+      if(a >= 0) {
+        if(iIndexValue >= jIndexValue) {
+          sorted(index) = iIndexValue
+          i += 1
+        }
+        else {
+          sorted(index) = jIndexValue
+          j -= 1
+        }
+        index -= 1
+      }
+      else {
+        if(iIndexValue >= jIndexValue) {
+          sorted(index) = jIndexValue
+          i += 1
+        }
+        else {
+          sorted(index) = iIndexValue
+          j -= 1
+        }
+        index -= 1
+      }
+    }
+    sorted
+  }
 }
