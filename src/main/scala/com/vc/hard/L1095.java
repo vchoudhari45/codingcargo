82c1c3c81e277dde81fb54919d053e3f0dbdd62d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-07-15 02:39:11 -0700

    L1095

--- src/main/scala/com/vc/hard/L1095.java
+++ src/main/scala/com/vc/hard/L1095.java
@@ -8,8 +8,53 @@ interface MountainArray {
     public int length();
 }
 
-class L1095 {
-    public int findInMountainArray(int target, MountainArray mountainArr) {
+class L0195 {
+    public int findInMountainArray(int target, MountainArray arr) {
+        int l = 0;
+        int r = arr.length() - 1;
+        int mid = 0;
 
+        //Find Peak
+        while(l <= r) {
+            mid = l + (r - l) / 2;
+            if(arr.get(mid) < arr.get(mid + 1)) {
+                l = mid + 1;
+            }
+            else r = mid - 1;
+        }
+
+        int peak = l;
+
+
+        //Find element in increasing Array using binary Search
+        l = 0;
+        r = peak;
+        while(l <= r) {
+            mid = l + (r - l) / 2;
+            int element = arr.get(mid);
+            if(element > target) {
+                r = mid - 1;
+            }
+            else if(element < target) {
+                l = mid + 1;
+            }
+            else return mid;
+        }
+
+        //Find element in decreasing Array using binary Search
+        l = peak;
+        r = arr.length() - 1;
+        while(l <= r) {
+            mid = l + (r - l) / 2;
+            int element = arr.get(mid);
+            if(element > target) {
+                l = mid + 1;
+            }
+            else if(element < target) {
+                r = mid - 1;
+            }
+            else return mid;
+        }
+        return -1;
     }
 }
