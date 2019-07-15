1c6c96a074e7c4f911178e9c7815bd5262f4b540
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-07-15 02:26:47 -0700

    L852

--- src/main/scala/com/vc/easy/L852.scala
+++ src/main/scala/com/vc/easy/L852.scala
@@ -2,14 +2,15 @@ package com.vc.easy
 
 object L852 {
   def peakIndexInMountainArray(arr: Array[Int]): Int = {
-    var max = arr(0)
-    var maxIndex = 0
-    arr.indices.foreach(i => {
-      if(max < arr(i)) {
-        max = arr(i)
-        maxIndex = i
+    var l = 0
+    var h = arr.length - 1
+    while(l <= h) {
+      val mid = l + (h - l) / 2
+      if(arr(mid) < arr(mid + 1)) {
+        l = mid + 1
       }
-    })
-    maxIndex
+      else h = mid - 1
+    }
+    l
   }
 }
---
+++ src/main/scala/com/vc/hard/L1095.java
@@ -0,0 +1,15 @@
+package com.vc.hard;
+
+
+// This is MountainArray's API interface.
+// You should not implement it, or speculate about its implementation
+interface MountainArray {
+    public int get(int index);
+    public int length();
+}
+
+class L1095 {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+
+    }
+}
