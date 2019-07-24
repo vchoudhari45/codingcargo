2ec946b64f15daa986552186723b68e8b5491bc6
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-07-24 04:01:59 -0700

    L1028

---
+++ src/main/scala/com/vc/hard/L1028.scala
@@ -0,0 +1,38 @@
+package com.vc.hard
+
+
+class TreeNode1028(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode1028 = null
+  var right: TreeNode1028 = null
+}
+
+object Solution {
+  def recoverFromPreorder(s: String): TreeNode1028 = {
+    import scala.collection.mutable
+    val map = new mutable.HashMap[Int, TreeNode1028]()
+    var i = 0
+    val n = s.length
+    while(i < n) {
+      var level = 0
+      while(s(i) == '-'){
+        level += 1
+        i += 1
+      }
+
+      var num = 0
+      while(i < n && s(i) != '-') {
+        num = num * 10 + s(i) - '0'
+        i += 1
+      }
+      val child = new TreeNode1028(num)
+      if(level != 0) {
+        val parent = map(level - 1)
+        if(parent.left == null) parent.left = child
+        else parent.right = child
+      }
+      map.put(level, child)
+    }
+    map(0)
+  }
+}
