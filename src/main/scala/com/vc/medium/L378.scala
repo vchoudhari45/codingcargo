d6dc8c9bb83746fa9ab5831f1a19e0c371ef9487
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-08-22 18:43:05 -0700

    L378

--- src/main/scala/com/vc/medium/L378.scala
+++ src/main/scala/com/vc/medium/L378.scala
@@ -2,32 +2,30 @@ package com.vc.medium
 
 object L378 {
   def kthSmallest(matrix: Array[Array[Int]], k: Int): Int = {
+    import scala.collection.mutable
+    object PQOrdering extends Ordering[(Int, Int)] {
+      def compare(p1:(Int, Int), p2: (Int, Int)): Int = {
+        matrix(p2._1)(p2._2).compareTo(matrix(p1._1)(p1._2))
+      }
+    }
+
+    val pq = new mutable.PriorityQueue[(Int, Int)]()(PQOrdering)
     val n = matrix.length
     if(n == 0) return 0
     val m = matrix(0).length
-    var lo = matrix(0)(0)
-    var hi = matrix(n - 1)(m - 1)
-    while(lo <= hi) {
-      val mid = lo + (hi - lo) / 2
-      val count = getLess(matrix, mid)
-      if(count < k) lo = mid + 1
-      else hi = mid - 1
-    }
-    lo
-  }
 
-  //Check diagonally from bottom-left to top-right
-  def getLess(arr: Array[Array[Int]], mid: Int): Int = {
-    val n = arr.length
-    val m = arr(0).length
-    var i = 0
-    var j = m - 1
-    var res = 0
-    while(i < n) {
-      while(j >= 0 && arr(i)(j) > mid) j -= 1
-      res += (j + 1)
+    //Insert first column in Priority Queue
+    matrix.indices.foreach(i => {
+      pq.enqueue((i, 0))
+    })
+
+    var i = 1
+    while(i < k) {
+      val e = pq.dequeue()
+      println(s"$i => ${matrix(e._1)(e._2)}")
+      if(e._2 < m - 1) pq.enqueue((e._1, e._2 + 1))
       i += 1
     }
-    res
+    matrix(pq.head._1)(pq.head._2)
   }
 }
