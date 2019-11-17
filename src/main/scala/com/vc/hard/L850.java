e06eb6cae3f57a892620867feb3a84268e59893b
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-11-16 19:21:11 -0800

    L850

--- src/main/scala/com/vc/hard/L850.java
+++ src/main/scala/com/vc/hard/L850.java
@@ -2,23 +2,21 @@ package com.vc.hard;
 
 import java.util.*;
 
-class IntervalY {
-    int[] y;
-    boolean isXEnd;
-    public IntervalY(int[] y, boolean isXEnd) {
-        this.y = y;
-        this.isXEnd = isXEnd;
-    }
+class Point {
+    int x;
+    int y;
+    int endValue;
 
-    @Override
-    public String toString() {
-        return y[0]+" "+y[1]+" "+isXEnd;
+    public Point(int x, int y, int endValue) {
+        this.x = x;
+        this.y = y;
+        this.endValue = endValue;
     }
 }
 
 class Solution {
     public int rectangleArea(int[][] rectangles) {
-        TreeMap<Integer, List<IntervalY>> map = new TreeMap<Integer, List<IntervalY>>();
+        List<Point> data = new ArrayList<Point>();
         for(int[] r: rectangles) {
             int x1 = r[0];
             int y1 = r[1];
@@ -26,69 +24,60 @@ class Solution {
             int x2 = r[2];
             int y2 = r[3];
 
-            List<IntervalY> listStart = map.getOrDefault(x1, new ArrayList<IntervalY>());
-            List<IntervalY> listEnd = map.getOrDefault(x2, new ArrayList<IntervalY>());
+            /** bottom-left 1 */
+            data.add(new Point(x1, y1, 1));
 
-            listStart.add(new IntervalY(new int[]{y1, y2}, false));
-            listEnd.add(new IntervalY(new int[]{y1, y2}, true));
+            /** top-left -1 */
+            data.add(new Point(x1, y2, -1));
 
-            map.put(x1, listStart);
-            map.put(x2, listEnd);
+            /** bottom-right should be oppposite of bottom-left so -1*/
+            data.add(new Point(x2, y1, -1));
+
+            /** top-right should be opposite of top-left so 1 */
+            data.add(new Point(x2, y2, 1));
         }
 
-        // for(Map.Entry<Integer, List<IntervalY>> entry: map.entrySet()) {
-        //     System.out.println(entry.getKey()+" "+entry.getValue());
+        Collections.sort(data, new Comparator<Point>(){
+            public int compare(Point p1, Point p2) {
+                return p1.x - p2.x;
+            }
+        });
+
+        // for(Point p: data) {
+        //     System.out.println(p.x+" "+p.y+" "+p.endValue);
         // }
 
-        long prevX = map.firstKey();
-        long sum = 0;
-        int MOD = (int)1e9 + 7;
         TreeMap<Integer, Integer> mapY = new TreeMap<Integer, Integer>();
-        for(Integer x: map.keySet()) {
-            int yDist = calculateYInterval(mapY);
-            sum += (x - prevX) % MOD * yDist % MOD;
-            sum %= MOD;
-            prevX = x;
-
-            List<IntervalY> ys = map.get(x);
-            for(IntervalY intervalY: ys) {
-                int[] y = intervalY.y;
-
-                int yStart = y[0];
-                int yEnd = y[1];
-
-                Integer count = mapY.get(yStart);
-                if(count == null) count = 0;
-                count += intervalY.isXEnd ? -1 : 1;
-                if(count == 0) mapY.remove(yStart);
-                else mapY.put(yStart, count);
-
-                count = mapY.get(yEnd);
-                if(count == null) count = 0;
-                count += intervalY.isXEnd ? 1 : -1;
-                if(count == 0) mapY.remove(yEnd);
-                else mapY.put(yEnd, count);
+        long preX = -1;
+        long preY = -1;
+        long result = 0;
+        int MOD = (int)1e9 + 7;
+        for(int i = 0; i < data.size(); i++) {
+            Point p = data.get(i);
+            mapY.put(p.y, mapY.getOrDefault(p.y, 0) + p.endValue);
+            if(i == data.size() - 1 || data.get(i + 1).x > p.x) {
+                if(preX > -1) {
+                    result += (preY * (p.x - preX)) % MOD;
+                    result %= MOD;
+                }
+                preX = p.x;
+                preY = calcY(mapY);
             }
         }
-        return (int)sum;
+        return (int)result;
     }
 
-    public int calculateYInterval(TreeMap<Integer, Integer> map) {
-        int res = 0;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+    public int calcY(TreeMap<Integer, Integer> map) {
+        int result = 0;
         int count = 0;
-        for(Integer y: map.keySet()) {
-            boolean isEnd = map.get(y) < 0;
-            count += map.get(y);
-            if(isEnd) max = Math.max(max, y);
-            else min = Math.min(min, y);
-            if(count == 0) {
-                res += max - min;
-                max = Integer.MIN_VALUE;
-                min = Integer.MAX_VALUE;
+        int pre = -1;
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if(count > 0) {
+                result += entry.getKey() - pre;
             }
+            count += entry.getValue();
+            pre = entry.getKey();
         }
-        return res;
+        return result;
     }
 }
