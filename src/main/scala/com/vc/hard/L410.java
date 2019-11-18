fad772d27aaf602608566e601178d943548b3455
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-11-18 12:57:46 -0800

    L410

--- src/main/scala/com/vc/hard/L410.java
+++ src/main/scala/com/vc/hard/L410.java
@@ -2,15 +2,17 @@ package com.vc.hard;
 
 class L410 {
     public int splitArray(int[] arr, int m) {
+        /**
+         Minimize the maximum
+         */
         int lo = Integer.MIN_VALUE;
         int hi = 0;
-
         for(int i = 0; i < arr.length; i++) {
             lo = Math.max(lo, arr[i]);
             hi += arr[i];
         }
 
-        if(m == 1) return (int)hi;
+        if(m == 1) return hi;
 
         while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
