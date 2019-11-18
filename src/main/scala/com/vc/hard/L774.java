79ce8a2d1f50329845e4755a68440b6e16896fb1
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-11-18 14:08:11 -0800

    L774

--- src/main/scala/com/vc/hard/L774.java
+++ src/main/scala/com/vc/hard/L774.java
@@ -1,21 +1,28 @@
 package com.vc.hard;
 
-import java.util.*;
-
-class L744 {
-    public double minmaxGasDist(int[] stations, int K) {
-        Arrays.sort(stations);
+class Solution {
+    public double minmaxGasDist(int[] arr, int K) {
         double lo = 0;
-        double hi = stations[stations.length - 1] - stations[0];
+        double hi = arr[arr.length - 1] - arr[0];
+        /**
+         Minimize the maximum
+         */
         while(lo + 1e-6 < hi) {
             double mid = lo + (hi - lo) / 2;
-            int count = 0;
-            for(int i = 0; i < stations.length - 1; i++) {
-                count += Math.ceil((stations[i + 1] - stations[i]) / mid) - 1;
-            }
-            if(count > K) lo = mid;
+            boolean isPossible = isPossible(arr, mid, K);
+            //System.out.println("lo: "+lo+" hi: "+hi+" Mid: "+mid+" isPossible: "+isPossible);
+            if(!isPossible) lo = mid + 1e-6;
             else hi = mid;
         }
-        return hi;
+        return lo;
+    }
+
+    private boolean isPossible(int[] arr, double mid, int k) {
+        int count = 0;
+        for(int i = 0; i < arr.length - 1; i++) {
+            count += Math.ceil((arr[i + 1] - arr[i]) / mid) - 1;
+            if(count > k) return false;
+        }
+        return true;
     }
 }
