50708cbd835b8e748365cd7a3e70ec09575a5ed8
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-11-18 00:56:03 -0800

    L410

---
+++ src/main/scala/com/vc/hard/L410.java
@@ -0,0 +1,36 @@
+package com.vc.hard;
+
+class L410 {
+    public int splitArray(int[] arr, int m) {
+        int lo = Integer.MIN_VALUE;
+        int hi = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            lo = Math.max(lo, arr[i]);
+            hi += arr[i];
+        }
+
+        if(m == 1) return (int)hi;
+
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(isValid(arr, m, mid)) hi = mid;
+            else lo = mid + 1;
+        }
+        return (int)lo;
+    }
+
+    private boolean isValid(int[] arr, int m, int sum) {
+        int total = 0;
+        int count = 1;
+        for(int i = 0; i < arr.length; i++) {
+            total += arr[i];
+            if(total > sum) {
+                total = arr[i];
+                count++;
+                if(count > m) return false;
+            }
+        }
+        return true;
+    }
+}
