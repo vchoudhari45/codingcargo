e03cd7567d384de7bba7f0d427ec3d00354678b5
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-11-18 15:26:28 -0800

    L528

---
+++ src/main/scala/com/vc/medium/L528.java
@@ -0,0 +1,38 @@
+package com.vc.medium;
+
+import java.util.*;
+
+//https://www.youtube.com/watch?v=KAZM4tsH8aI
+class Solution {
+
+    int[] wSum;
+    Random r;
+    int len;
+
+    public Solution(int[] w) {
+        len = w.length;
+        r = new Random();
+        for(int i = 1; i < len; i++) {
+            w[i] += w[i - 1];
+        }
+        this.wSum = w;
+    }
+
+    public int pickIndex() {
+        int randomSum = r.nextInt(wSum[len - 1]);
+        int lo = 0;
+        int hi = len - 1;
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(wSum[mid] <= randomSum) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
