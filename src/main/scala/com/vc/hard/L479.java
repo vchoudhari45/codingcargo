622b61afca21f73a265ef94fd0f844a459971006
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-11-25 19:58:02 -0800

    L479

--- src/main/scala/com/vc/hard/L479.java
+++ src/main/scala/com/vc/hard/L479.java
@@ -4,40 +4,54 @@ import java.util.*;
 
 class L479 {
     public int largestPalindrome(int n) {
-        if(n == 1) return 9;
-        long high = (int)Math.pow(10, n) - 1;
-        long lo = (int)Math.pow(10, n - 1);
-        long product = high * high;
-        long firstHalf = (int)(product / Math.pow(10, n));
-        //System.out.println("high: "+high+" low: "+lo+" product: "+product+" firstHalf: "+firstHalf);
-        while(firstHalf > lo) {
-            List<Long> p = generatePalindrome(firstHalf--);
-            for(Long palindrome: p) {
-                //System.out.println("palindrome: "+palindrome+" firstHalf: "+firstHalf);
-                if(palindrome > product) continue;
-                for(long factor1 = high; factor1 >= lo; factor1--) {
-                    long factor2 = palindrome / factor1;
-                    if(factor2 > high) break;
-                    if(palindrome % factor1 == 0 &&  factor2 >= lo && factor2 <= high && factor2 != factor1) {
-                        //System.out.println("palindrome: "+palindrome+" factor1: "+factor1+" factor2: "+factor2);
-                        return (int)(palindrome % 1337L);
+        long lo = n > 1 ? Long.parseLong(repeat("9", n - 1)) : 0;
+        long hi = Long.parseLong(repeat("9", n));
+        long prevPalindrome = hi * hi + 1;
+        long maxProduct = prevPalindrome;
+        //System.out.println(prevPalindrome+" "+maxProduct);
+        while(prevPalindrome > lo) {
+            prevPalindrome = getPrevPalindrome(prevPalindrome);
+            //System.out.println(prevPalindrome);
+            if(prevPalindrome < maxProduct) {
+                for(long factor1 = hi; factor1 > lo; factor1--) {
+                    boolean isDivisible = prevPalindrome % factor1 == 0;
+                    long factor2 = prevPalindrome / factor1;
+                    if(factor2 > hi) break;
+                    if(isDivisible && factor2 > lo && factor2 <= hi && factor1 != factor2) {
+                        //System.out.println("Factor1: "+factor1+" Factor2: "+factor2);
+                        return (int)(prevPalindrome % 1337L);
                     }
                 }
             }
+            prevPalindrome--;
         }
-        return -1;
+        return 1;
     }
 
-    private List<Long> generatePalindrome(long firstHalf) {
-        List<Long> res = new ArrayList<Long>();
-        StringBuilder str = new StringBuilder(Long.toString(firstHalf));
-        String firstPart = str.toString();
-        String secondPart = str.reverse().toString();
-        String r = firstPart+secondPart;
-        res.add(Long.parseLong(r));
-        for(char c = '0'; c <='9'; c++) {
-            res.add(Long.parseLong(firstPart+c+secondPart));
-        }
-        return res;
+    private long getPrevPalindrome(Long maxProduct) {
+        if(maxProduct <= 10) return --maxProduct;
+        else if(maxProduct == 11) return 9;
+
+        String str = Long.toString(maxProduct);
+        int len = (str.length() + 1) / 2;
+
+        StringBuilder firstBuilder = new StringBuilder(str.substring(0, len));
+        String first = firstBuilder.toString();
+        String firstReverse = firstBuilder.reverse().toString();
+        long cand1 = Long.parseLong(first + firstReverse);
+
+        long prev = Long.parseLong(first) - 1;
+        StringBuilder secondBuilder = new StringBuilder(Long.toString(prev));
+        String second = secondBuilder.toString();
+        String secondReverse = secondBuilder.reverse().toString();
+        long cand2 = Long.parseLong(second + secondReverse);
+
+        return Math.min(cand1, cand2);
+    }
+
+    private String repeat(String a, int n) {
+        String str = "";
+        for(int i = 0; i < n; i++) str += a;
+        return str;
     }
 }
