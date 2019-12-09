defffa02880f4382f9cec2acd3980fc44facb182
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-12-09 03:07:51 -0800

    L42

---
+++ src/main/scala/com/vc/hard/L42.java
@@ -0,0 +1,25 @@
+package com.vc.hard;
+
+class L42 {
+    public int trap(int[] arr) {
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        int res = 0;
+        int maxLeft = 0;
+        int maxRight = 0;
+        while(left <= right) {
+            if(arr[left] <= arr[right]) {
+                if(maxLeft < arr[left]) maxLeft = arr[left];
+                else res += maxLeft - arr[left];
+                left++;
+            }
+            else {
+                if(maxRight < arr[right]) maxRight = arr[right];
+                else res += maxRight - arr[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
