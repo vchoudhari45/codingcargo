3feab361944d081772dbb6ea4661e505d334f303
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-12-08 17:14:11 -0800

    L37

--- src/main/scala/com/vc/hard/L32.java
+++ src/main/scala/com/vc/hard/L32.java
@@ -1,25 +1,27 @@
 package com.vc.hard;
 
-import java.util.*;
-
 class L32 {
     public int longestValidParentheses(String s) {
-        Stack<Integer> st = new Stack<Integer>();
+        /**
+         ((()))
+         012345
+
+         (()())
+         012345
+         */
         int n = s.length();
-        int i = 0;
-        int start = 0;
+        int[] dp = new int[n];
         int max = 0;
-        while(i < n) {
-            char ch = s.charAt(i);
-            if(ch == '(') st.push(i);
-            else {
-                if(st.isEmpty()) start = i + 1;
-                else {
-                    st.pop();
-                    max = st.isEmpty() ? Math.max(max, i - start + 1) : Math.max(max, i - st.peek());
+        for(int i = 1; i < n; i++) {
+            if(s.charAt(i) == ')') {
+                if(s.charAt(i - 1) == '(') {
+                    dp[i] = ((i >= 2) ? dp[i - 2] : 0) + 2;
+                }
+                else if(i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1] >= 2) ? dp[i - dp[i - 1] - 2] : 0) + 2;
                 }
             }
-            i++;
+            max = Math.max(max, dp[i]);
         }
         return max;
     }
