1597b852ec48aba3cbaa8f790673f34595edde9d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-12-12 04:49:41 -0800

    L154

---
+++ src/main/java/com/vc/hard/L154.java
@@ -0,0 +1,15 @@
+package com.vc.hard;
+
+class L154 {
+    public int findMin(int[] arr) {
+        int lo = 0;
+        int hi = arr.length - 1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(arr[mid] < arr[hi]) hi = mid;
+            else if(arr[mid] > arr[hi]) lo = mid + 1;
+            else hi--;
+        }
+        return arr[lo];
+    }
+}
