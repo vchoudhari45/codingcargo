406dbfc5cb31d77358e7ddcd516ac5a4fba757b8
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-12-18 22:49:46 -0800

    L403

--- src/main/java/com/vc/hard/L391.java
+++ src/main/java/com/vc/hard/L391.java
@@ -3,50 +3,42 @@ package com.vc.hard;
 import java.util.*;
 
 class L391 {
-    public boolean isRectangleCover(int[][] rectangles) {
+    public boolean isRectangleCover(int[][] rectangle) {
+        HashSet<String> set = new HashSet<>();
         int x1 = Integer.MAX_VALUE;
-        int y1 = Integer.MAX_VALUE;
         int x2 = Integer.MIN_VALUE;
+        int y1 = Integer.MAX_VALUE;
         int y2 = Integer.MIN_VALUE;
+        int area = 0;
 
-        long area = 0;
-        HashSet<String> set = new HashSet<String>();
-        for(int[] r: rectangles) {
-            x1 = Math.min(x1, r[0]);
-            y1 = Math.min(y1, r[1]);
-            x2 = Math.max(x2, r[2]);
-            y2 = Math.max(y2, r[3]);
+        for(int[] arr: rectangle) {
+            x1 = Math.min(x1, arr[0]);
+            y1 = Math.min(y1, arr[1]);
+            x2 = Math.max(x2, arr[2]);
+            y2 = Math.max(y2, arr[3]);
 
-            String s1 = r[0] +" "+ r[1];
-            String s2 = r[0] +" "+ r[3];
-            String s3 = r[2] +" "+ r[1];
-            String s4 = r[2] +" "+ r[3];
+            String s1 = arr[0] + " " + arr[1];
+            String s2 = arr[2] + " " + arr[3];
+            String s3 = arr[0] + " " + arr[3];
+            String s4 = arr[2] + " " + arr[1];
 
-            if(!set.contains(s1)) set.add(s1);
-            else set.remove(s1);
+            if(!set.add(s1)) set.remove(s1);
+            if(!set.add(s2)) set.remove(s2);
+            if(!set.add(s3)) set.remove(s3);
+            if(!set.add(s4)) set.remove(s4);
 
-            if(!set.contains(s2)) set.add(s2);
-            else set.remove(s2);
-
-            if(!set.contains(s3)) set.add(s3);
-            else set.remove(s3);
-
-            if(!set.contains(s4)) set.add(s4);
-            else set.remove(s4);
-
-            area += (r[2] - r[0]) * (r[3] - r[1]);
+            area += (arr[2] - arr[0]) * (arr[3] - arr[1]);
         }
 
-        String s1 = x1 +" "+ y1;
-        String s2 = x1 +" "+ y2;
-        String s3 = x2 +" "+ y1;
-        String s4 = x2 +" "+ y2;
-
-        System.out.println(set);
+        String s1 = x1 + " " + y1;
+        String s2 = x2 + " " + y2;
+        String s3 = x1 + " " + y2;
+        String s4 = x2 + " " + y1;
 
         if(!set.contains(s1)
-                || !set.contains(s2) || !set.contains(s3) || !set.contains(s4) || set.size() != 4) return false;
+                || !set.contains(s2) || !set.contains(s3) || !set.contains(s4)
+                || set.size() != 4) return false;
 
-        return area == (x2 - x1) * (y2 - y1);
+        return (x2 - x1) * (y2 - y1) == area;
     }
 }
--- src/main/java/com/vc/hard/L403.java
+++ src/main/java/com/vc/hard/L403.java
@@ -2,16 +2,13 @@ package com.vc.hard;
 
 class L403 {
     public boolean canCross(int[] stones) {
-        //0,1,3,5,6,8,12,17
         int n = stones.length;
-        //dp[i][j] denotes if frog can make a jump of size j from stone at index i
-        boolean dp[][] = new boolean[n][n + 1];
+        boolean[][] dp = new boolean[n + 1][n + 1];
         dp[0][1] = true;
-
         for(int i = 1; i < n; i++) {
             for(int j = 0; j < i; j++) {
                 int diff = stones[i] - stones[j];
-                if(diff < 0 || diff > n || !dp[j][diff]) continue;
+                if(diff < 0 || diff >= n || !dp[j][diff]) continue;
                 dp[i][diff] = true;
                 if(diff - 1 >= 0) dp[i][diff - 1] = true;
                 if(diff + 1 < n) dp[i][diff + 1] = true;
