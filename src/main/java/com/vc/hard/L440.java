bef84948be43a7ceb026d47b79ac84a05799a241
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-12-20 06:04:42 -0800

    L440

--- src/main/java/com/vc/hard/L440.java
+++ src/main/java/com/vc/hard/L440.java
@@ -1,30 +1,29 @@
 package com.vc.hard;
 
 class L440 {
+    private int res = 0;
+    private int current = 0;
     public int findKthNumber(int n, int k) {
-        int curr = 1;
-        k = k - 1;
-        while(k > 0) {
-            int step = findSteps(n, curr, curr + 1);
-            if(step <= k) {
-                curr += 1;
-                k -= step;
-            }
-            else {
-                curr *= 10;
-                k -= 1;
-            }
+        for(char ch = '1'; ch <= '9'; ch++) {
+            solve(ch + "", n, k);
         }
-        return curr;
+        return res;
     }
 
-    private int findSteps(int n, long n1, long n2) {
-        int step = 0;
-        while(n1 <= n) {
-            step += Math.min(n + 1, n2) - n1;
-            n1 *= 10;
-            n2 *= 10;
+    private void solve(String num, int n, int k) {
+        int number = Integer.parseInt(num);
+        if(number > n) return;
+        else {
+            current++;
+            //System.out.println("number: "+number+" current: "+current);
+            if(current == k) {
+                res = number;
+            }
+            else {
+                for(char ch = '0'; ch <= '9'; ch++) {
+                    solve(num + ch, n, k);
+                }
+            }
         }
-        return step;
     }
 }
