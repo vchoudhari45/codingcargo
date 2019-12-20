ccf80d0a23b7f8469d3fa9fd994261bb7b746be9
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-12-20 03:14:51 -0800

    L425

--- src/main/java/com/vc/hard/L425.java
+++ src/main/java/com/vc/hard/L425.java
@@ -4,53 +4,43 @@ import java.util.*;
 
 class L425 {
     public List<List<String>> wordSquares(String[] words) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(int i = 0; i < words.length; i++) {
-            String word = words[i];
+        HashMap<String, List<String>> word = new HashMap<>();
+        for(String w: words) {
             String prefix = "";
-            for(int j = 0; j < word.length(); j++) {
-                char ch = word.charAt(j);
-                prefix += ch;
-                List<String> list = map.getOrDefault(prefix, new ArrayList<String>());
-                list.add(word);
-                map.put(prefix, list);
+            for(int i = 0; i <= w.length(); i++) {
+                prefix = w.substring(0, i);
+                List<String> list = word.getOrDefault(prefix, new ArrayList<String>());
+                list.add(w);
+                word.put(prefix, list);
             }
         }
-        //System.out.println(map);
+        //System.out.println(word);
         List<List<String>> res = new ArrayList<List<String>>();
-        List<String> ans = new ArrayList<String>();
-        for(String s: words) {
-            ans.add(s);
-            searchNext(words, map, ans, res);
-            ans.remove(ans.size() - 1);
-        }
+        if(words.length == 0) return res;
+        List<String> l = new ArrayList<String>();
+        solve(0, words, word, words[0].length(), l, res);
         return res;
     }
 
-    private void searchNext(String[] words,
-                            Map<String, List<String>> map,
-                            List<String> ans,
-                            List<List<String>> res) {
-        if(words[0].length() == ans.size()) {
-            res.add(new ArrayList<String>(ans));
+    private void solve(int row, String[] words,
+                       HashMap<String, List<String>> word,
+                       int length, List<String> l, List<List<String>> res) {
+        if(row == length) {
+            res.add(new ArrayList<String>(l));
         }
         else {
             String prefix = "";
-            int index = ans.size();
-            for(String str: ans) {
-                prefix += str.charAt(index);
+            for(String str: l) {
+                prefix += str.charAt(row);
             }
-            //System.out.println("Searching for prefix: "+prefix+" ans: "+ans);
-            List<String> startingWithPrefix = map.get(prefix);
-            if(startingWithPrefix != null && startingWithPrefix.size() > 0) {
-                for(String eachStartingWithPrefix: startingWithPrefix) {
-                    ans.add(eachStartingWithPrefix);
-                    //System.out.println("Ans: "+ ans);
-                    searchNext(words, map, ans, res);
-                    ans.remove(ans.size() - 1);
+            if(word.containsKey(prefix)) {
+                List<String> wordsWithPrefix = word.get(prefix);
+                for(String wordWithPrefix: wordsWithPrefix) {
+                    l.add(wordWithPrefix);
+                    solve(row + 1, words, word, length, l, res);
+                    l.remove(l.size() - 1);
                 }
             }
-            //System.out.println("After prefix: "+prefix+" ans: "+ans+" res: "+res);
         }
     }
 }
