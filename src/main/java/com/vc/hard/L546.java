d413aab37755d4cb65bf2f80e751c02dadb2ea1c
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2019-12-28 02:26:58 -0800

    L546

--- src/main/java/com/vc/hard/L546.java
+++ src/main/java/com/vc/hard/L546.java
@@ -1,31 +1,27 @@
 package com.vc.hard;
 
 class L546 {
+    int[][][] m;
     public int removeBoxes(int[] boxes) {
+        if(boxes == null || boxes.length == 0) return 0;
         int n = boxes.length;
-        int dp[][][] = new int[n][n][n];
-        return solve(0, n - 1, 0, boxes, dp);
+        m = new int[n][n][n];
+        return dfs(boxes, 0, n - 1, 0);
     }
 
-    public int solve(int left, int right, int same, int[]boxes, int[][][] dp) {
-        if(left > right) return 0;
-        if(dp[left][right][same] > 0) return dp[left][right][same];
-
-        while(left + 1 <= right && boxes[left + 1] == boxes[left]) {
-            same++;
-            left++;
+    private int dfs(int[] boxes, int l, int r, int k) {
+        if(l > r) return 0;
+        while(r > 0 && boxes[r] == boxes[r - 1]) {
+            r--;
+            k++;
         }
-
-        dp[left][right][same] = (same + 1) * (same + 1) + solve(left + 1, right, 0, boxes, dp);
-
-        for(int k = left + 1; k <= right; k++) {
-            if(boxes[k] == boxes[left]) {
-                dp[left][right][same] = Math.max(
-                        dp[left][right][same],
-                        solve(left + 1, k - 1, 0, boxes, dp) + solve(k, right, same + 1, boxes, dp)
-                );
+        if(m[l][r][k] > 0) return m[l][r][k];
+        m[l][r][k] = dfs(boxes, l, r - 1, 0) + (k + 1) * (k + 1);
+        for(int i = l; i < r; i++) {
+            if(boxes[i] == boxes[r]) {
+                m[l][r][k] = Math.max(m[l][r][k], dfs(boxes, l, i, k + 1) + dfs(boxes, i + 1, r - 1, 0));
             }
         }
-        return dp[left][right][same];
+        return m[l][r][k];
     }
 }
