85c4e20ca8837fa6bb07e17021ab13918112bc27
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-01 00:53:14 -0800

    L699

--- src/main/java/com/vc/hard/L699.java
+++ src/main/java/com/vc/hard/L699.java
@@ -4,26 +4,31 @@ import java.util.*;
 
 class L699 {
     public List<Integer> fallingSquares(int[][] positions) {
-        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         map.put(0, 0);
+        List<Integer> res = new ArrayList<>();
         int maxHeight = 0;
-        List<Integer> list = new ArrayList<Integer>();
-        for(int[] pos: positions) {
-            int start = pos[0];
-            int end = start + pos[1];
-            int from = map.floorKey(start);
-            int height = map.subMap(from, end).values().stream().max(Integer::compare).orElse(0) + pos[1];
+        for(int i = 0; i < positions.length; i++) {
+            int start = positions[i][0];
+            int end = positions[i][1] + start;
 
-            maxHeight = Math.max(height, maxHeight);
-            list.add(maxHeight);
+            int from = map.floorKey(start);
+            Map<Integer, Integer> subMap = map.subMap(from, true, end, false);
+            int height = 0;
+            for(Map.Entry<Integer, Integer> entry: subMap.entrySet()) {
+                height = Math.max(height, entry.getValue());
+            }
+            height += positions[i][1];
+            maxHeight = Math.max(maxHeight, height);
             int lastHeight = map.floorEntry(end).getValue();
+            res.add(maxHeight);
 
             map.put(start, height);
             map.put(end, lastHeight);
 
-            Map<Integer, Integer> subMap = map.subMap(start, false, end, false);
-            map.keySet().removeAll(new HashSet<Integer>(subMap.keySet()));
+            subMap = map.subMap(start, false, end, false);
+            map.keySet().removeAll(new HashSet<>(subMap.keySet()));
         }
-        return list;
+        return res;
     }
 }
