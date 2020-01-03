e364586a5c72b7ad059cd92a8353fdc3378f0dbc
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-03 05:00:25 -0800

    L719

--- src/main/java/com/vc/hard/L719.java
+++ src/main/java/com/vc/hard/L719.java
@@ -4,21 +4,24 @@ import java.util.Arrays;
 
 class L719 {
     public int smallestDistancePair(int[] arr, int k) {
-        int n = arr.length;
         Arrays.sort(arr);
         int lo = 0;
-        int hi = arr[n - 1] - arr[0];
-        while (lo < hi) {
+        int hi = arr[arr.length - 1] - arr[0];
+        while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            int count = 0;
-            int left = 0;
-            for (int right = 0; right < n; right++) {
-                while (arr[right] - arr[left] > mid) left++;
-                count += right - left;
-            }
-            if (count >= k) hi = mid;
-            else lo = mid + 1;
+            if(isSmaller(arr, mid, k)) lo = mid + 1;
+            else hi = mid;
         }
         return lo;
+    }
+
+    private boolean isSmaller(int[] arr, int mid, int k) {
+        int count = 0;
+        int left = 0;
+        for(int right = 0; right < arr.length; right++) {
+            while(arr[right] - arr[left] > mid) left++;
+            count += right - left;
+        }
+        return count < k;
     }
 }
