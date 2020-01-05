4fa704679037fa3eab5c6fc4e7f18abc62577939
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-05 06:20:42 -0800

    L757

--- src/main/java/com/vc/hard/L757.java
+++ src/main/java/com/vc/hard/L757.java
@@ -4,15 +4,19 @@ import java.util.*;
 
 class L757 {
     public int intersectionSizeTwo(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> {
-            return a[1] - b[1];
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] x, int[] y) {
+                return Integer.valueOf(x[1]).compareTo(y[1]);
+            }
         });
 
-        int min = -1, max = -1, ans = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int ans = 0;
         for(int i = 0; i < intervals.length; i++) {
             int start = intervals[i][0];
             int end = intervals[i][1];
-            //System.out.println(start+" "+ end);
+
             if(start > max) {
                 ans += 2;
                 min = end - 1;
@@ -20,9 +24,13 @@ class L757 {
             }
             else if(start > min) {
                 ans++;
-                min = max == end ? max - 1: max;
+                min = max == end ? end - 1 : max;
                 max = end;
             }
+            else if(start < min) {
+                //Not possible intervals are sorted by end of the interval
+            }
+            //System.out.println("start: "+start+" end: "+end+" min: "+min+" max: "+max);
         }
         return ans;
     }
