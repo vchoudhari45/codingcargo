256dbb19e0df236e68729b69d8a958d3705b8ef5
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-07 04:10:51 -0800

    L793

--- src/main/java/com/vc/hard/L793.java
+++ src/main/java/com/vc/hard/L793.java
@@ -2,33 +2,30 @@ package com.vc.hard;
 
 class L793 {
     //https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/discuss/117821/Four-binary-search-solutions-based-on-different-ideas
-    public int preimageSizeFZF(int K) {
-        long numbersWithKMinusOneTrailingZero = numbersWithKTrailingZero(K - 1);
-        long numbersWithKTrailingZero = numbersWithKTrailingZero(K);
-        return (int) (numbersWithKTrailingZero - numbersWithKMinusOneTrailingZero);
+    public int preimageSizeFZF(int k) {
+        long countNext = numbersWithKTrailingZeros(k + 1);
+        long countPrev = numbersWithKTrailingZeros(k);
+        //System.out.println(countNext+" "+countPrev);
+        return (int)(countNext - countPrev);
     }
 
-    private long numbersWithKTrailingZero(int K) {
+    private long numbersWithKTrailingZeros(int k) {
         long lo = 0;
-        long hi =  5L * (K + 1);
-        while(lo <= hi) {
+        long hi = 5 * (k + 1);
+        while(lo < hi) {
             long mid = lo + (hi - lo) / 2;
-            long zerosForMid = numberOfTraillingZerosFor(mid);
-            if(zerosForMid > K) {
-                hi = mid - 1;
-            }
-            else if(zerosForMid <= K) {
-                lo = mid + 1;
-            }
+            int zerosForMid = numbersOfZeros(mid);
+            if(zerosForMid < k) lo = mid + 1;
+            else hi = mid;
         }
         return lo;
     }
 
-    private long numberOfTraillingZerosFor(long x) {
-        long res = 0;
-        while(x > 0) {
-            res += x / 5;
-            x = x / 5;
+    private int numbersOfZeros(long n) {
+        int res = 0;
+        while(n > 0) {
+            res += n / 5;
+            n /= 5;
         }
         return res;
     }
