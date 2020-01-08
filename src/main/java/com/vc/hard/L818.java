77f38de0ad549bd856735c6e1322b4253ed1498d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-08 04:56:09 -0800

    L818

--- src/main/java/com/vc/hard/L818.java
+++ src/main/java/com/vc/hard/L818.java
@@ -3,31 +3,53 @@ package com.vc.hard;
 import java.util.*;
 
 class L818 {
-    public int racecar(int target) {
-        Queue<int[]> q = new LinkedList<int[]>();
-        q.offer(new int[]{0, 1}); //position, speed
+    static class State {
+        int position = 0;
+        int speed = 0;
+        State(int position, int speed) {
+            this.position = position;
+            this.speed = speed;
+        }
 
-        int level = 0;
-        HashSet<String> visited = new HashSet<String>();
+        @Override
+        public String toString() {
+            return "position: "+position+" speed: "+speed;
+        }
+    }
+
+    public int racecar(int target) {
+        State initial = new State(0, 1);
+        Queue<State> q = new LinkedList<>();
+        q.add(initial);
+
+        HashSet<String> visited = new HashSet<>();
+        int count = 0;
         while(!q.isEmpty()) {
             int size = q.size();
             for(int i = 0; i < size; i++) {
-                int[] state = q.poll();
-                String key = state[0] +" "+ state[1];
-                if(state[0] == target) return level;
-                if(!visited.contains(key) && 0 < state[0] + state[1] && state[0] + state[1] < (target << 1)) {
-                    //forward
-                    q.offer(new int[] {state[0] + state[1], state[1] * 2});
+                State state = q.poll();
+
+                //System.out.println(state);
+
+                if(state.position == target) return count;
+
+                String stateKey = state.position+" "+state.speed;
+                if(visited.contains(stateKey)) continue;
+                visited.add(stateKey);
+
+                //Try A
+                if(0 < state.position + state.speed && state.position + state.speed < (target << 1)){
+                    State newStateA = new State(state.position + state.speed, state.speed * 2);
+                    q.offer(newStateA);
                 }
 
-                if(!visited.contains(key)) {
-                    //backward
-                    q.offer(new int[] {state[0], state[1] > 0 ? -1 : 1});
-                }
-                visited.add(key);
+                //Try R
+                State newStateR = new State(state.position, state.speed > 0 ? -1 : 1);
+                q.offer(newStateR);
+
             }
-            level++;
+            count++;
         }
-        return level;
+        return -1;
     }
 }
