5ae33e8db4be0d05db6a979a976a9a995a6bbaba
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-09 01:24:01 -0800

    L839

---
+++ src/main/java/com/vc/hard/L839.java
@@ -0,0 +1,45 @@
+package com.vc.hard;
+
+class L839 {
+    public int numSimilarGroups(String[] arr) {
+        int n = arr.length;
+        if(n == 0) return 0;
+        int[] parentArr = new int[n];
+
+        for(int i = 0; i < parentArr.length; i++) parentArr[i] = i;
+
+        int res = n;
+        for(int i = 1; i < arr.length; i++){
+            for(int j = 0; j < i; j++) {
+                //System.out.println("\nComparing "+arr[i]+" with "+arr[j]);
+                if(isSimilar(i, j, arr)) {
+                    //System.out.println(arr[i]+" and "+arr[j]+" are similar");
+                    int pi = find(i, parentArr);
+                    int pj = find(j, parentArr);
+                    parentArr[pi] = pj;
+                    if(pi != pj) res--;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private boolean isSimilar(int i, int j, String[] arr) {
+        int count = 0;
+        String s1 = arr[i];
+        String s2 = arr[j];
+        for(int index = 0; index < s1.length(); index++) {
+            if(s1.charAt(index) != s2.charAt(index)) {
+                count++;
+                if(count > 2) return false;
+            }
+        }
+        return true;
+    }
+
+    private int find(int p, int[] parentArr) {
+        if(parentArr[p] == p) return p;
+        else return find(parentArr[p], parentArr);
+    }
+}
