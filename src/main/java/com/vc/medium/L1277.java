eca516eb04163b100b5e1233b5365e51a66e9a23
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-11 10:13:33 -0800

    L1277

---
+++ src/main/java/com/vc/medium/L1277.java
@@ -0,0 +1,21 @@
+package com.vc.medium;
+
+class L1277 {
+    public int countSquares(int[][] matrix) {
+        int n = matrix.length;
+        if(n == 0) return 0;
+        int m = matrix[0].length;
+
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] > 0 && i > 0 && j > 0) {
+                    matrix[i][j] = Math.min(Math.min(matrix[i - 1][j - 1], matrix[i - 1][j]),
+                            matrix[i][j - 1]) + 1;
+                }
+                res += matrix[i][j];
+            }
+        }
+        return res;
+    }
+}
