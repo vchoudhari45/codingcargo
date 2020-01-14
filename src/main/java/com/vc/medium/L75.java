16ad9b3b4bea777cedd11537fef66c131d097b2e
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-13 23:46:21 -0800

    L75

--- src/main/java/com/vc/medium/L75.java
+++ src/main/java/com/vc/medium/L75.java
@@ -2,18 +2,22 @@ package com.vc.medium;
 
 class L75 {
     public void sortColors(int[] arr) {
-        int r = 0;
-        int w = 0;
-        int b = 0;
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == 0) r++;
-            else if(arr[i] == 1) w++;
-            else b++;
+        int low = 0;
+        int high = arr.length - 1;
+        for(int i = low; i <= high;) {
+            if(arr[i] == 0) {
+                swap(i++, low++, arr);
+            }
+            else if(arr[i] == 2) {
+                swap(i, high--, arr);
+            }
+            else i++;
         }
-        for(int i = 0; i < arr.length; i++) {
-            if(r-- > 0) arr[i] = 0;
-            else if(w-- > 0) arr[i] = 1;
-            else arr[i] = 2;
-        }
+    }
+
+    private void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j]= temp;
     }
 }
