1d068f0e45b528dbda18dcf1315859f33a4b78a9
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-15 08:26:00 -0800

    L593

---
+++ src/main/java/com/vc/medium/L593.java
@@ -0,0 +1,20 @@
+package com.vc.medium;
+
+import java.util.*;
+
+class L593 {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(distance(p1, p2));
+        set.add(distance(p1, p3));
+        set.add(distance(p1, p4));
+        set.add(distance(p2, p3));
+        set.add(distance(p2, p4));
+        set.add(distance(p3, p4));
+        return set.size() == 2 && !set.contains(0);
+    }
+
+    private int distance(int[] p1, int[] p2) {
+        return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
+    }
+}
