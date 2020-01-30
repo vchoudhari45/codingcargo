94fff818e0fb6585ba9681e9827923bcb5aa8995
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-01-30 15:43:54 -0800

    L215

---
+++ src/main/java/com/vc/medium/L215.java
@@ -0,0 +1,14 @@
+package com.vc.medium;
+
+import java.util.PriorityQueue;
+
+class L215 {
+    public int findKthLargest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < arr.length; i++) {
+            pq.offer(arr[i]);
+            if(pq.size() > k) pq.poll();
+        }
+        return pq.poll();
+    }
+}
