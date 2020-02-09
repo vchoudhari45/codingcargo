26c7080c2dabb49a7d5a6d349e5216e58c124f00
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-09 07:16:38 -0800

    L943

--- src/main/java/com/vc/hard/L943.java
+++ src/main/java/com/vc/hard/L943.java
@@ -3,68 +3,69 @@ package com.vc.hard;
 import java.util.*;
 
 class L943 {
-    //https://leetcode.com/problems/find-the-shortest-superstring/discuss/194932/Travelling-Salesman-Problem
     public String shortestSuperstring(String[] arr) {
+        //https://leetcode.com/problems/find-the-shortest-superstring/discuss/194932/Travelling-Salesman-Problem
         int n = arr.length;
         int[][] graph = new int[n][n];
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++) {
                 graph[i][j] = calc(arr[i], arr[j]);
                 graph[j][i] = calc(arr[j], arr[i]);
             }
         }
 
-        int dp[][] = new int[1 << n][n];
-        int path[][] = new int[1 << n][n];
+        int[][] dp = new int[1 << n][n];
+        int[][] path = new int[1 << n][n];
         int min = Integer.MAX_VALUE;
         int last = -1;
-        for(int i = 0; i < (1 << n); i++) { //foreach state
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-            for(int j = 0; j < n; j++) {  //calculate how this state can be reached from each node
-                if((i & (1 << j)) > 0) {
-                    int prev = i - (1 << j);
+        for(int to = 0; to < (1 << n); to++) { //for each state
+            Arrays.fill(dp[to], Integer.MAX_VALUE);
+            for(int from = 0; from < n; from++) {
+                if((to & (1 << from)) > 0) { //if ith state has jth Node?, if yes see how it can be reached
+                    int prev = to - (1 << from);
                     if(prev == 0) {
-                        dp[i][j] = arr[j].length();
-                    } else {
-                        for(int k = 0; k < n; k++) {
-                            if(dp[prev][k] < Integer.MAX_VALUE && dp[prev][k] + graph[k][j] < dp[i][j]) {
-                                dp[i][j] = dp[prev][k] + graph[k][j];
-                                path[i][j] = k;
+                        dp[to][from] = arr[from].length();
+                    }
+                    else {
+                        for(int via = 0; via < n; via++) {
+                            if(dp[prev][via] != Integer.MAX_VALUE
+                                    && dp[prev][via] + graph[via][from] < dp[to][from]) {
+                                dp[to][from] = dp[prev][via] + graph[via][from];
+                                path[to][from] = via;
                             }
                         }
                     }
                 }
-                if(i == (1 << n) - 1 && min > dp[i][j]) {
-                    min = dp[i][j];
-                    last = j;
+                if(to == (1 << n) - 1 && min > dp[to][from]) {
+                    min = dp[to][from];
+                    last = from;
                 }
             }
         }
 
-        Stack<Integer> st = new Stack<Integer>();
-        int cur = (1 << n) - 1;
-        while(cur > 0) {
+        Stack<Integer> st = new Stack<>();
+        int current = (1 << n) - 1;
+        while(current > 0) {
             st.push(last);
-            int temp = cur;
-            cur -= (1 << last);
-            last = path[temp][last];
+            int next = current;
+            current -= (1 << last);
+            last = path[next][last];
         }
 
-        StringBuilder str = new StringBuilder();
-        int i = st.pop();
-        str.append(arr[i]);
+        StringBuilder sb = new StringBuilder();
+        int from = st.pop();
+        sb.append(arr[from]);
         while(!st.isEmpty()) {
-            int j = st.pop();
-            str.append(arr[j].substring(arr[j].length() - graph[i][j]));
-            i = j;
+            int to = st.pop();
+            sb.append(arr[to].substring(arr[to].length() - graph[from][to]));
+            from = to;
         }
-        return str.toString();
+        return sb.toString();
     }
 
     private int calc(String a, String b) {
+        //abcd bcde
         for(int i = 0; i < a.length(); i++) {
-            //gcta ctaagt
-            //alex lex
             if(b.startsWith(a.substring(i))) return b.length() - a.length() + i;
         }
         return b.length();
