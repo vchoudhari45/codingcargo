755a471a6a273230279158764367879de2ea996d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-10 05:18:37 -0800

    L996

--- src/main/java/com/vc/hard/L995.java
+++ src/main/java/com/vc/hard/L995.java
@@ -1,4 +1,22 @@
 package com.vc.hard;
 
-public class L995 {
+class L995 {
+    public int minKBitFlips(int[] arr, int K) {
+        int n = arr.length;
+        int flipped = 0;
+        int[] isFlipped = new int[n];
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            if(i >= K) {
+                flipped ^= isFlipped[i - K];
+            }
+            if(flipped == 0 && arr[i] == 0 || flipped == 1 && arr[i] == 1) {
+                if(i + K > arr.length) return -1;
+                flipped ^= 1;
+                isFlipped[i] = 1;
+                res++;
+            }
+        }
+        return res;
+    }
 }
---
+++ src/main/java/com/vc/hard/L996.java
@@ -0,0 +1,61 @@
+package com.vc.hard;
+
+import java.util.*;
+
+class L996 {
+    int count = 0;
+    public int numSquarefulPerms(int[] arr) {
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                int from = arr[i];
+                int to = arr[j];
+
+                double sqrt = Math.sqrt(from + to);
+                if(sqrt == Math.ceil(sqrt)) {
+                    HashSet<Integer> fromList = map.getOrDefault(from, new HashSet<Integer>());
+                    fromList.add(to);
+
+                    HashSet<Integer> toList = map.getOrDefault(to, new HashSet<Integer>());
+                    toList.add(from);
+
+                    map.put(from, fromList);
+                    map.put(to, toList);
+                }
+            }
+        }
+
+        //System.out.println(map);
+
+        //{1=[8], 17=[8], 8=[1, 17]}
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for(int i = 0; i < arr.length; i++) {
+            countMap.put(arr[i], countMap.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for(Integer from: countMap.keySet()) {
+            solve(from, arr.length - 1, map, countMap);
+        }
+        return count;
+    }
+
+    private void solve(int from, int remaining,
+                       HashMap<Integer, HashSet<Integer>> map, HashMap<Integer, Integer> countMap) {
+        if(countMap.getOrDefault(from, 0) <= 0) return;
+
+        if(remaining == 0) {
+            //System.out.println("Increamenting");
+            count++;
+        }
+
+        //System.out.println(from);
+        countMap.put(from, countMap.get(from) - 1);
+        if(map.containsKey(from)) {
+            for(Integer to: map.get(from)) {
+                solve(to, remaining - 1, map, countMap);
+            }
+        }
+        countMap.put(from, countMap.get(from) + 1);
+    }
+}
