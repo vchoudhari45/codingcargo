845f59d2533a547bf854ab08ce76a1600b27493f
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-11 11:10:21 -0800

    L1192

--- src/main/java/com/vc/hard/L1192.java
+++ src/main/java/com/vc/hard/L1192.java
@@ -4,52 +4,65 @@ import java.util.*;
 
 /**
  *  Basically, it uses dfs to travel through the graph to find if current vertex u, can travel back to u or previous vertex
- *  low[u] records the lowest vertex u can reach
- *  disc[u] records the time when u was discovered
+ *  lowest[u] records the lowest vertex u can reach
+ *  discovered[u] records the time when u was discovered
  **/
 class L1192 {
+    int[] discovered, lowest;
+    int times = 0; // time when discover each vertex
+    HashMap<Integer, List<Integer>> graph;
+    List<List<Integer>> res;
+
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        int[] disc = new int[n], low = new int[n];
-        // use adjacency list instead of matrix will save some memory, adjmatrix will cause MLE
-        List<Integer>[] graph = new ArrayList[n];
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.fill(disc, -1); // use disc to track if visited (disc[i] == -1)
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        // build graph
-        for (int i = 0; i < connections.size(); i++) {
-            int from = connections.get(i).get(0), to = connections.get(i).get(1);
-            graph[from].add(to);
-            graph[to].add(from);
+        discovered = new int[n];
+        lowest = new int[n];
+        Arrays.fill(discovered, -1);
+        res = new ArrayList<>();
+
+        graph = new HashMap<>();
+        for(List<Integer> connection: connections) {
+            int from = connection.get(0);
+            int to = connection.get(1);
+
+            List<Integer> fromList = graph.getOrDefault(from, new ArrayList<Integer>());
+            List<Integer> toList = graph.getOrDefault(to, new ArrayList<Integer>());
+
+            fromList.add(to);
+            toList.add(from);
+
+            graph.put(from, fromList);
+            graph.put(to, toList);
         }
 
-        for (int i = 0; i < n; i++) {
-            if (disc[i] == -1) {
-                dfs(i, low, disc, graph, res, i);
+        //System.out.println(graph);
+        for(int i = 0; i < n; i++) {
+            if(discovered[i] == -1) {
+                dfs(i, i);
             }
         }
         return res;
     }
 
-    int time = 0; // time when discover each vertex
+    private void dfs(int current, int parent) {
+        discovered[current] = lowest[current] = times++;
 
-    private void dfs(int u, int[] low, int[] disc, List<Integer>[] graph, List<List<Integer>> res, int pre) {
-        disc[u] = low[u] = ++time; // discover u
-        for (int j = 0; j < graph[u].size(); j++) {
-            int v = graph[u].get(j);
-            if (v == pre) {
-                continue; // if parent vertex, ignore
-            }
-            if (disc[v] == -1) { // if not discovered
-                dfs(v, low, disc, graph, res, u);
-                low[u] = Math.min(low[u], low[v]);
-                if (low[v] > disc[u]) {
-                    // u - v is critical, there is no path for v to reach back to u or previous vertices of u
-                    res.add(Arrays.asList(u, v));
+        if(graph.containsKey(current)) {
+            for(Integer child: graph.get(current)) {
+                if(child == parent) continue; // if parent vertex, that means edge is in cycle
+
+                if(discovered[child] == -1) {
+                    dfs(child, current);
+                    lowest[current] = Math.min(lowest[current], lowest[child]);
+                    if(lowest[child] > discovered[current]) {
+                        //child node is not a part of any cycle otherwise
+                        //lowest node it could reach was at least current
+                        res.add(Arrays.asList(current, child));
+                    }
                 }
-            } else { // if v discovered and is not parent of u, update low[u], cannot use low[v] because u is not subtree of v
-                low[u] = Math.min(low[u], disc[v]);
+                else {
+                    // if child is already discovered and is not parent of current, update lowest[current]
+                    lowest[current] = Math.min(lowest[current], lowest[child]);
+                }
             }
         }
     }
