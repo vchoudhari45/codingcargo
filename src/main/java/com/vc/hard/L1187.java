67f7665977f7213f4118d1516328eacebd19b290
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-11 04:38:24 -0800

    L1187

--- src/main/java/com/vc/hard/L1187.java
+++ src/main/java/com/vc/hard/L1187.java
@@ -2,33 +2,48 @@ package com.vc.hard;
 
 import java.util.*;
 
+//TLE: Work on fixing it
+
 class L1187 {
+    HashMap<String, Integer> memo = new HashMap<>();
+
     public int makeArrayIncreasing(int[] arr1, int[] arr2) {
-        TreeSet<Integer> arr2Sorted = new TreeSet<Integer>();
-        for(int i = 0; i < arr2.length; i++) {
-            arr2Sorted.add(arr2[i]);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int i = 0; i < arr2.length; i++) map.put(arr2[i], map.getOrDefault(arr2[i], 0) + 1);
+
+        int res = solve(0, Integer.MIN_VALUE, arr1, map);
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+
+    private int solve(int index1, int prev, int[] arr1, TreeMap<Integer, Integer> arr2) {
+        if(index1 == arr1.length) return 0;
+
+        String key = index1+" "+prev;
+        if(memo.containsKey(key)) memo.get(key);
+
+        //keep the current index
+        int keep = Integer.MAX_VALUE;
+        if(arr1[index1] > prev) {
+            keep = solve(index1 + 1, arr1[index1], arr1, arr2);
         }
 
-        int n = arr1.length;
-        int[][] dp = new int[n + 1][n + 1];
-        for(int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-        dp[0][0] = Integer.MIN_VALUE;
+        //replace the current index
+        int replace = Integer.MAX_VALUE;
+        if(!arr2.isEmpty()) {
+            Integer replaceElement = arr2.higherKey(prev);
+            if(replaceElement != null) {
+                arr2.put(replaceElement, arr2.get(replaceElement) - 1);
+                if(arr2.get(replaceElement) == 0) arr2.remove(replaceElement);
 
-        for(int j = 1; j < dp.length; j++) {
-            for(int i = 0; i <= j; i++) {
-                if(arr1[j - 1] > dp[i][j - 1]) {
-                    dp[i][j] = arr1[j - 1];
-                }
+                replace = solve(index1 + 1, replaceElement, arr1, arr2);
+                if(replace != Integer.MAX_VALUE) replace++;
 
-                if(i > 0 && arr2Sorted.higher(dp[i - 1][j - 1]) != null) {
-                    dp[i][j] = Math.min(dp[i][j], arr2Sorted.higher(dp[i - 1][j - 1]));
-                }
-
-                if(j == dp.length - 1 && dp[i][j] != Integer.MAX_VALUE) return i;
+                arr2.put(replaceElement, arr2.getOrDefault(replaceElement, 0) + 1);
             }
         }
-        return -1;
+
+        int res = Math.min(keep, replace);
+        memo.put(key, res);
+        return res;
     }
 }
