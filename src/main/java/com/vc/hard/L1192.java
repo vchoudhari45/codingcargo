469a7cc2a153ab0f114cd4ff887e1b1d54c98b13
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-11 05:44:45 -0800

    L1192

--- src/main/java/com/vc/hard/L1192.java
+++ src/main/java/com/vc/hard/L1192.java
@@ -2,41 +2,54 @@ package com.vc.hard;
 
 import java.util.*;
 
+/**
+ *  Basically, it uses dfs to travel through the graph to find if current vertex u, can travel back to u or previous vertex
+ *  low[u] records the lowest vertex u can reach
+ *  disc[u] records the time when u was discovered
+ **/
 class L1192 {
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        List<Integer> graph[] = new ArrayList[n];
-        for(int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<Integer>();
+        int[] disc = new int[n], low = new int[n];
+        // use adjacency list instead of matrix will save some memory, adjmatrix will cause MLE
+        List<Integer>[] graph = new ArrayList[n];
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.fill(disc, -1); // use disc to track if visited (disc[i] == -1)
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        for(int i = 0; i < connections.size(); i++) {
-            int from = connections.get(i).get(0);
-            int to = connections.get(i).get(1);
+        // build graph
+        for (int i = 0; i < connections.size(); i++) {
+            int from = connections.get(i).get(0), to = connections.get(i).get(1);
             graph[from].add(to);
             graph[to].add(from);
         }
-        int[] disc = new int[n];
-        int[] low = new int[n];
-        dfs(0, -1, graph, disc, low);
+
+        for (int i = 0; i < n; i++) {
+            if (disc[i] == -1) {
+                dfs(i, low, disc, graph, res, i);
+            }
+        }
         return res;
     }
 
-    int time = 0;
-    List<List<Integer>> res = new ArrayList<List<Integer>>();
-    private void dfs(int from, int prev, List<Integer> graph[], int[] disc, int[] low) {
-        disc[from] = ++time;
-        low[from] = time;
-        for(int i = 0; i < graph[from].size(); i++) {
-            int to = graph[from].get(i);
-            if(to ==prev) continue;
-            if(disc[to] == 0) {
-                dfs(to, from, graph, disc, low);
-                low[from] = Math.min(low[from], low[to]);
-                if(low[to] > disc[from]) {
-                    res.add(Arrays.asList(from, to));
-                }
+    int time = 0; // time when discover each vertex
+
+    private void dfs(int u, int[] low, int[] disc, List<Integer>[] graph, List<List<Integer>> res, int pre) {
+        disc[u] = low[u] = ++time; // discover u
+        for (int j = 0; j < graph[u].size(); j++) {
+            int v = graph[u].get(j);
+            if (v == pre) {
+                continue; // if parent vertex, ignore
             }
-            else {
-                low[from] = Math.min(low[from], low[to]);
+            if (disc[v] == -1) { // if not discovered
+                dfs(v, low, disc, graph, res, u);
+                low[u] = Math.min(low[u], low[v]);
+                if (low[v] > disc[u]) {
+                    // u - v is critical, there is no path for v to reach back to u or previous vertices of u
+                    res.add(Arrays.asList(u, v));
+                }
+            } else { // if v discovered and is not parent of u, update low[u], cannot use low[v] because u is not subtree of v
+                low[u] = Math.min(low[u], disc[v]);
             }
         }
     }
