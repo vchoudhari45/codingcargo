c02aef76f295c363e5c8abdf9252917debb92494
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-12 08:43:41 -0800

    L1335

--- src/main/java/com/vc/hard/L1335.java
+++ src/main/java/com/vc/hard/L1335.java
@@ -6,23 +6,23 @@ class L1335 {
         for(int i = 0; i < dp.length; i++) {
             for(int j = 0; j < dp[0].length; j++) dp[i][j] = -1;
         }
-        int res = solve(jobs, 0, d, 0, dp);
+        int res = solve(jobs, 0, d, dp);
         return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    private int solve(int[] jobs, int finished, int d, int prevMax, int[][] dp) {
+    private int solve(int[] jobs, int finished, int d, int[][] dp) {
         if(dp[finished][d] != -1) return dp[finished][d];
         if(d == 0) {
-            if(finished == jobs.length) return prevMax;
+            if(finished == jobs.length) return 0;
             else return Integer.MAX_VALUE;
         }
         else if(finished == jobs.length) return Integer.MAX_VALUE;
 
         int res = Integer.MAX_VALUE;
-        int difficulty = prevMax;
+        int difficulty = 0;
         for(int i = finished; i < jobs.length; i++) {
             difficulty = Math.max(difficulty, jobs[i]);
-            int ans = solve(jobs, i + 1, d - 1, 0, dp);
+            int ans = solve(jobs, i + 1, d - 1, dp);
             if(ans != Integer.MAX_VALUE) res = Math.min(res, ans + difficulty);
         }
         dp[finished][d] = res;
