a321aa811911474acc9f93045b0eb7a26ac1bf66
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-12 03:19:30 -0800

    L1269

--- src/main/java/com/vc/hard/L1269.java
+++ src/main/java/com/vc/hard/L1269.java
@@ -3,26 +3,25 @@ package com.vc.hard;
 import java.util.*;
 
 class L1269 {
+    int[][] memo;
     int MOD = (int)1e9 + 7;
     public int numWays(int steps, int arrLen) {
-        if(arrLen == 1) return 1;
-
         if(arrLen > steps) arrLen = steps;
-        int[][] dp = new int[steps + 1][arrLen + 1];
-        for(int i = 0; i < dp.length; i++) Arrays.fill(dp[i], -1);
-        return solve(steps, 0, arrLen, dp);
+        this.memo = new int[steps + 1][arrLen + 1];
+        for(int i = 0; i < memo.length; i++) Arrays.fill(memo[i], -1);
+        return solve(steps, 0, arrLen);
     }
 
-    private int solve(int steps, int pos, int len, int[][] dp) {
-        if(pos >= len || pos < 0) return 0;
-        if(steps == 0) return pos == 0 ? 1 : 0;
-        if(dp[steps][pos] != -1) return dp[steps][pos];
+    private int solve(int steps, int index, int arrLen) {
+        if(index >= arrLen || index < 0) return 0;
+        if(steps == 0) return index == 0 ? 1 : 0;
+        if(memo[steps][index] != -1) return memo[steps][index];
 
-        long right = solve(steps - 1, pos + 1, len, dp) % MOD;  //right
-        long left = solve(steps - 1, pos - 1, len, dp) % MOD;  //left
-        long stay = solve(steps - 1, pos, len, dp) % MOD;      //stay
+        long left = solve(steps - 1, index - 1, arrLen) % MOD;
+        long right = solve(steps - 1, index + 1, arrLen) % MOD;
+        long stay = solve(steps - 1, index , arrLen) % MOD;
 
-        dp[steps][pos] = (int)((right + left + stay) % MOD);
-        return dp[steps][pos];
+        memo[steps][index] = (int)((left + right + stay) % MOD);
+        return memo[steps][index];
     }
 }
