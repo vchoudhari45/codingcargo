18c5b370539328f7b3cabd241d1de70a41140704
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-13 22:08:46 -0800

    L400

---
+++ src/main/java/com/vc/medium/L400.java
@@ -0,0 +1,20 @@
+package com.vc.medium;
+
+class L400 {
+    public int findNthDigit(int n) {
+        int len = 1;
+        long count = 9;
+        int start = 1;
+
+        while (n > len * count) {
+            n -= len * count;
+            len += 1;
+            count *= 10;
+            start *= 10;
+        }
+
+        start += (n - 1) / len;
+        String s = Integer.toString(start);
+        return (s.charAt((n - 1) % len)) - '0';
+    }
+}
