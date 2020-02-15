e3d21ce5c5e7af7e0ae71d8500edd6c17dc03a5a
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-14 23:40:09 -0800

    L777

--- src/main/java/com/vc/medium/L777.java
+++ src/main/java/com/vc/medium/L777.java
@@ -2,31 +2,45 @@ package com.vc.medium;
 
 class L777 {
     public boolean canTransform(String start, String end) {
+        if (!start.replace("X", "").equals(end.replace("X", "")))
+            return false;
+
         int p1 = 0;
         int p2 = 0;
-        if(!start.replaceAll("X", "").equals(end.replaceAll("X", ""))) return false;
 
-        //"R" can move to the right until it is blocked by "L" while "L" can move to the left until it is blocked by "R".
-        while(p1 < start.length() && p2 < end.length()) {
-            while(p1 < start.length() && start.charAt(p1) == 'X') p1++;
+        while(p1 < start.length() && p2 < end.length()){
 
-            while(p2 < end.length() && end.charAt(p2) == 'X') p2++;
+            // get the non-X positions of 2 strings
+            while(p1 < start.length() && start.charAt(p1) == 'X'){
+                p1++;
+            }
+            while(p2 < end.length() && end.charAt(p2) == 'X'){
+                p2++;
+            }
 
-            if(p1 == start.length() && p2 == end.length()) return true;
+            //if both of the pointers reach the end the strings are transformable
+            if(p1 == start.length() && p2 == end.length()){
+                return true;
+            }
+            // if only one of the pointer reach the end they are not transformable
+            if(p1 == start.length() || p2 == end.length()){
+                return false;
+            }
 
-            if(p1 == start.length() || p2 == end.length()) return false;
-
-            if(start.charAt(p1) != end.charAt(p2)) return false;
-
-            if(start.charAt(p1) == 'L' && p1 < p2) return false;
-
-            if(start.charAt(p1) == 'R' && p1 > p2) return false;
-
+            if(start.charAt(p1) != end.charAt(p2)){
+                return false;
+            }
+            // if the character is 'L', it can only be moved to the left. p1 should be greater or equal to p2.
+            if(start.charAt(p1) == 'L' && p2 > p1){
+                return false;
+            }
+            // if the character is 'R', it can only be moved to the right. p2 should be greater or equal to p1.
+            if(start.charAt(p1) == 'R' && p1 > p2){
+                return false;
+            }
             p1++;
             p2++;
         }
-        while(p1 < start.length() && start.charAt(p1) == 'X') p1++;
-        while(p2 < start.length() && end.charAt(p2) == 'X') p2++;
-        return p1 == start.length() && p2 == end.length();
+        return true;
     }
 }
