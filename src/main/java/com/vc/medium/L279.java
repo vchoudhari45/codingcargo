a78567bfe9d9b6da6674a74a8951774efafe8671
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-16 01:01:50 -0800

    L279

--- src/main/java/com/vc/medium/L279.java
+++ src/main/java/com/vc/medium/L279.java
@@ -1,14 +1,31 @@
 package com.vc.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class L279 {
+    private Map<Integer, Integer> memo;
+
     public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        for(int i = 0; i < dp.length; i++) dp[i] = i;
-        for(int i = 0; i <= n; i++) {
-            for(int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+        memo = new HashMap<>();
+        return numSquaresFrom(n);
+    }
+
+    private int numSquaresFrom(int target) {
+        if (target == 0 || target == 1) {
+            return target;
+        }
+
+        if (memo.containsKey(target)) return memo.get(target);
+
+        int minSquares = Integer.MAX_VALUE;
+        for(int base = 1; base <= target / 2; base++) {
+            if (base * base <= target) {
+                minSquares = Math.min(minSquares, numSquaresFrom(target - base * base) + 1);
             }
         }
-        return dp[n];
+
+        memo.put(target, minSquares);
+        return minSquares;
     }
 }
