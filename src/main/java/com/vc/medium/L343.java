284cb51ded819a4ae69102df31aa749c9cda64a9
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-17 00:42:59 -0800

    L343

---
+++ src/main/java/com/vc/medium/L343.java
@@ -0,0 +1,21 @@
+package com.vc.medium;
+
+import java.util.*;
+
+class L343 {
+    Map<Integer, Integer> memo = new HashMap<>();
+    public int integerBreak(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            res = Math.max(i * Math.max(n - i, integerBreak(n - i)), res);
+        }
+        memo.put(n, res);
+        return res;
+    }
+}
