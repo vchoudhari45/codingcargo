e9dac8f7d54e572f39b56c0bac6b04e19a26e544
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-18 02:50:04 -0800

    L421

---
+++ src/main/java/com/vc/medium/L421.java
@@ -0,0 +1,44 @@
+package com.vc.medium;
+
+class L421 {
+    class Trie {
+        Trie[] children;
+        public Trie() {
+            children = new Trie[2];
+        }
+    }
+
+    public int findMaximumXOR(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        // Init Trie.
+        Trie root = new Trie();
+        for(int num: nums) {
+            Trie curNode = root;
+            for(int i = 31; i >= 0; i --) {
+                int curBit = (num >>> i) & 1;
+                if(curNode.children[curBit] == null) {
+                    curNode.children[curBit] = new Trie();
+                }
+                curNode = curNode.children[curBit];
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int num: nums) {
+            Trie curNode = root;
+            int curSum = 0;
+            for(int i = 31; i >= 0; i --) {
+                int curBit = (num >>> i) & 1;
+                if(curNode.children[curBit ^ 1] != null) {
+                    curSum += (1 << i);
+                    curNode = curNode.children[curBit ^ 1];
+                }else {
+                    curNode = curNode.children[curBit];
+                }
+            }
+            max = Math.max(curSum, max);
+        }
+        return max;
+    }
+}
