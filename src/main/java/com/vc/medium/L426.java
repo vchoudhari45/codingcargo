8b7083c3fedd30f35761cc49d22edef1fa7c8150
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-18 12:30:37 -0800

    L426

--- src/main/java/com/vc/medium/L426.java
+++ src/main/java/com/vc/medium/L426.java
@@ -1,15 +1,13 @@
 package com.vc.medium;
 
-import java.util.Stack;
-
-class Node426 {
+class Node {
     public int val;
-    public Node426 left;
-    public Node426 right;
+    public Node left;
+    public Node right;
 
-    public Node426() {}
+    public Node() {}
 
-    public Node426(int _val,Node426 _left,Node426 _right) {
+    public Node(int _val,Node _left,Node _right) {
         val = _val;
         left = _left;
         right = _right;
@@ -17,27 +15,39 @@ class Node426 {
 }
 
 class L426 {
-    public Node426 treeToDoublyList(Node426 root) {
-        if(root == null) return null;
-        Node426 dummyNode = new Node426();
-        Node426 prev = dummyNode;
-        Node426 current = root;
-        Stack<Node426> st = new Stack<Node426>();
-        while(current != null || !st.isEmpty()) {
-            if(current != null) {
-                st.push(current);
-                current = current.left;
+    // the smallest (first) and the largest (last) nodes
+    Node first = null;
+    Node last = null;
+
+    public void helper(Node node) {
+        if (node != null) {
+            // left
+            helper(node.left);
+            // node
+            if (last != null) {
+                // link the previous node (last)
+                // with the current one (node)
+                last.right = node;
+                node.left = last;
             }
             else {
-                current = st.pop();
-                prev.right = current;
-                current.left = prev;
-                prev = current;
-                current = current.right;
+                // keep the smallest node
+                // to close DLL later on
+                first = node;
             }
+            last = node;
+            // right
+            helper(node.right);
         }
-        prev.right = dummyNode.right;
-        dummyNode.right.left = prev;
-        return dummyNode.right;
+    }
+
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+
+        helper(root);
+        // close DLL
+        last.right = first;
+        first.left = last;
+        return first;
     }
 }
