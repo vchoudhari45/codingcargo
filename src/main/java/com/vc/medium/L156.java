72f4d5785f80c445891d248b13a02b139d10def8
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-18 09:05:56 -0800

    L156

---
+++ src/main/java/com/vc/medium/L156.java
@@ -0,0 +1,16 @@
+package com.vc.medium;
+
+import com.vc.hard.TreeNode;
+
+class L156 {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null || root.left == null) return root;
+
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;   // node 2 left children
+        root.left.right = root;        // node 2 right children
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
