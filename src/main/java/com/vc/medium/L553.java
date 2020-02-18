a1cd8c947e51cffec73c90654341a0b3f81975a3
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-18 11:08:40 -0800

    L553

---
+++ src/main/java/com/vc/medium/L553.java
@@ -0,0 +1,17 @@
+package com.vc.medium;
+
+class L553 {
+    public String optimalDivision(int[] nums) {
+        if (nums.length == 1)
+            return nums[0] + "";
+        if (nums.length == 2)
+            return nums[0] + "/" + nums[1];
+        StringBuilder res = new StringBuilder(nums[0] + "/(" + nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            res.append("/" + nums[i]);
+        }
+        res.append(")");
+        return res.toString();
+    }
+}
+
