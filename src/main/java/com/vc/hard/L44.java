9c8613d94d854dcee865849bdd051ae75bc9be32
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-26 11:59:41 +0530

    L44

--- src/main/java/com/vc/hard/L44.java
+++ src/main/java/com/vc/hard/L44.java
@@ -1,22 +1,55 @@
 package com.vc.hard;
 
 class L44 {
+    int[][] dp;
     public boolean isMatch(String s, String p) {
-        int sn = s.length();
-        int pn = p.length();
-        boolean[][] dp = new boolean[sn + 1][pn + 1];
-        for(int i = 0; i <= sn; i++) {
-            for(int j = 0; j <= pn; j++) {
-                if(i == 0 && j == 0) dp[i][j] = true;
-                else if(j == 0) dp[i][j] = false;
-                else if(i == 0 && p.charAt(j - 1) == '*') dp[i][j] = dp[i][j - 1];
-                else if(i == 0) dp[i][j] = false;
-                else if(p.charAt(j - 1) == '*') dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i - 1][j - 1];
-                else if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?')
-                    dp[i][j] = dp[i - 1][j - 1];
-                else dp[i][j] = false;
+        this.dp = new int[s.length() + 1][p.length() + 1];
+        for(int i = 0; i < dp.length; i++)
+            for(int j = 0; j < dp[i].length; j++)
+                dp[i][j] = -1;
+
+        return solve(s.toCharArray(), 0, p.toCharArray(), 0);
+    }
+
+    private boolean solve(char[] s, int si, char[] p, int pi) {
+        if(si == s.length || pi == p.length) {
+            if(si == s.length && pi == p.length) {
+                dp[si][pi] = 1;
+                return true;
             }
+
+            if(si == s.length) {
+                while(pi < p.length && p[pi] == '*') pi++;
+                if(pi == p.length) {
+                    dp[si][pi] = 1;
+                    return true;
+                }
+                else {
+                    dp[si][pi] = 0;
+                    return false;
+                }
+            }
+            else return false;
         }
-        return dp[sn][pn];
+
+        if(dp[si][pi] != -1) return dp[si][pi] == 1;
+
+        boolean isMatch = false;
+        if(p[pi] == '*') {
+            //zero Match
+            if(solve(s, si, p, pi + 1)) isMatch = true;
+
+            //one Character Match
+            if(solve(s, si + 1, p, pi + 1)) isMatch = true;
+
+            //More than one character match
+            if(solve(s, si + 1, p, pi)) isMatch = true;
+        }
+
+        if(p[pi] == '?' || p[pi] == s[si])
+            isMatch = solve(s, si + 1, p, pi + 1);
+
+        dp[si][pi] = isMatch ? 1 : 0;
+        return isMatch;
     }
 }
