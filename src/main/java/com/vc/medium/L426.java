21ff47da3a5ed8675d8dec2519b82a22ebc2a75d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-28 16:40:20 +0530

    L426

--- src/main/java/com/vc/medium/L426.java
+++ src/main/java/com/vc/medium/L426.java
@@ -1,53 +1,43 @@
 package com.vc.medium;
 
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-
-    public Node() {}
-
-    public Node(int _val,Node _left,Node _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-}
-
 class L426 {
-    // the smallest (first) and the largest (last) nodes
-    Node first = null;
-    Node last = null;
 
-    public void helper(Node node) {
-        if (node != null) {
-            // left
-            helper(node.left);
-            // node
-            if (last != null) {
-                // link the previous node (last)
-                // with the current one (node)
-                last.right = node;
-                node.left = last;
-            }
-            else {
-                // keep the smallest node
-                // to close DLL later on
-                first = node;
-            }
-            last = node;
-            // right
-            helper(node.right);
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
         }
     }
 
-    public Node treeToDoublyList(Node root) {
-        if (root == null) return null;
+    private Node first, last;
 
-        helper(root);
-        // close DLL
+    public Node treeToDoublyList(Node root) {
+        if(root == null) return null;
+        solve(root);
         last.right = first;
         first.left = last;
         return first;
+    }
+
+    private void solve(Node node) {
+        if(node == null) return;
+
+        solve(node.left);
+        if(last != null) {
+            last.right = node;
+            node.left = last;
+        }
+        else {
+            first = node;
+        }
+        last = node;
+        solve(node.right);
     }
 }
