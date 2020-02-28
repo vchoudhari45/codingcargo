73bd71e697e6d98142096aa5562c8ed1ecb83094
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-29 02:17:41 +0530

    L328

---
+++ src/main/java/com/vc/medium/L328.java
@@ -0,0 +1,20 @@
+package com.vc.medium;
+
+import com.vc.hard.ListNode;
+
+class L328 {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) return null;
+
+        ListNode odd = head, even = head.next, evenHead = even;
+        while(even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
