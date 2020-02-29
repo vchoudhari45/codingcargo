e7c0d4d6867752c6e40a7c1cdc4469d1b7a65037
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-02-29 12:30:58 +0530

    L1252

---
+++ src/main/java/com/vc/easy/L1252.java
@@ -0,0 +1,21 @@
+package com.vc.easy;
+
+class L1252 {
+    public int oddCells(int n, int m, int[][] indices) {
+        int[] row = new int[n];
+        int[] col = new int[m];
+
+        for(int[] indice: indices) {
+            row[indice[0]]++;
+            col[indice[1]]++;
+        }
+
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(row[i] + col[j] % 2 != 0) count++;
+            }
+        }
+        return count;
+    }
+}
