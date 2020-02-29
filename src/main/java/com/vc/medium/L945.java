c5f2c266875496dfa5d0fdff5f3f04b9ab015df1
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-01 00:34:24 +0530

    L945

---
+++ src/main/java/com/vc/medium/L945.java
@@ -0,0 +1,18 @@
+package com.vc.medium;
+
+import java.util.Arrays;
+
+class L945 {
+    public int minIncrementForUnique(int[] A) {
+        if(A == null || A.length == 0) return 0;
+
+        Arrays.sort(A);
+        int next = 0, moves = 0, prev = A[0];
+        for(int i = 1; i < A.length; i++) {
+            next = prev + 1;
+            moves += A[i] > next ? 0 : next - A[i];
+            prev = Math.max(next, A[i]);
+        }
+        return moves;
+    }
+}
