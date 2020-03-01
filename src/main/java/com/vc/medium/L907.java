26601c6b141ed70e4c10b6cf4a9de34ff954b13d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-01 19:30:53 +0530

    L907

---
+++ src/main/java/com/vc/medium/L907.java
@@ -0,0 +1,44 @@
+package com.vc.medium;
+
+import java.util.Stack;
+
+class L907 {
+    public int sumSubarrayMins(int[] A) {
+        int len = A.length;
+        Stack<Integer> stack = new Stack<>();
+
+        int[] left = new int[len];
+        int[] right = new int[len];
+
+        for(int i = 0; i < A.length; i++) {
+            left[i] = i + 1;
+            right[i] = len - i;
+        }
+
+        // previous less element
+        for(int i = 0; i < len; i++){
+            while(!stack.isEmpty() && A[stack.peek()] > A[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+
+        //next less element
+        stack = new Stack<>();
+        for(int i = 0; i < len; i++){
+            while(!stack.isEmpty() && A[stack.peek()] > A[i]) {
+                right[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+
+        int ans = 0;
+        int mod = (int)1e9 + 7;
+        for(int i = 0; i < len; i++) {
+            ans = (ans + A[i] * left[i] * right[i]) % mod;
+        }
+        return ans;
+    }
+}
