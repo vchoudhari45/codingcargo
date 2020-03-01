f90845a997c80bf52008d61ff58b7f62f14c38d6
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-01 18:14:06 +0530

    L1366

---
+++ src/main/java/com/vc/medium/L1366.java
@@ -0,0 +1,34 @@
+package com.vc.medium;
+
+import java.util.*;
+
+class L1366 {
+    public String rankTeams(String[] votes) {
+        Map<Character, int[]> map = new HashMap<>();
+        int l = votes[0].length();
+        for(String vote : votes){
+            for(int i = 0; i < l; i++){
+                char c = vote.charAt(i);
+                map.putIfAbsent(c, new int[l]);
+                map.get(c)[i]++;
+            }
+        }
+
+        List<Character> list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> {
+            for (int i = 0; i < l; i++) {
+                if (map.get(a)[i] != map.get(b)[i]) {
+                    return map.get(b)[i] - map.get(a)[i];
+                }
+            }
+            return a - b;
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(char c : list){
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
+
