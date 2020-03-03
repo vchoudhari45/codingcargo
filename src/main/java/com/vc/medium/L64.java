75e78ee308bf36bc3d2b1d1176eaffac83c5cee9
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-03 22:50:16 +0530

    L64

---
+++ src/main/java/com/vc/medium/L64.java
@@ -0,0 +1,30 @@
+package com.vc.medium;
+
+class L64 {
+    public int minPathSum(int[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(i == 0 && j == 0) continue;
+
+                else if(i == 0) {
+                    grid[i][j] += grid[i][j - 1];
+                }
+
+                else if(j == 0) {
+                    grid[i][j] += grid[i - 1][j];
+                }
+
+                else {
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+
+        return grid[n - 1][m - 1];
+    }
+}
