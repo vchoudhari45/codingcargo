a9ae2963b3d4e940bcaf5bac699e1c7e5f4e7d00
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-04 22:02:12 +0530

    L1292

--- src/main/java/com/vc/medium/L1292.java
+++ src/main/java/com/vc/medium/L1292.java
@@ -5,6 +5,30 @@ class L1292 {
         int n = arr.length;
         int m = arr[0].length;
 
+        /**
+
+                0 1 2 3 4 5 6
+             0 [1 1 3 2 4 3 2]
+             1 [1 1 3 2 4 3 2]
+             2 [1 1 3 2 4 3 2]
+
+                0 1 2  3  4  5  6  7
+             0 [0 0 0  0  0  0  0  0]
+             1 [0 1 2  5  7 11 14 16]
+             2 [0 2 4 10 14 22 28 32]
+             3 [0 1 4 13 19 31 40 46]
+
+             side = Math.min(3, 7)  = 3
+
+             R1 = 0
+             C1 = 0
+
+             R2 = 3
+             C2 = 3
+
+             prefix[R2][C2] - prefix[R1][C2] - prefix[R2][C1] + prefix[R1][C1]
+         */
+
         int[][] prefix = new int[n + 1][m + 1];
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
@@ -12,24 +36,18 @@ class L1292 {
             }
         }
 
-        // for(int i = 0; i <= n; i++) {
-        //     for(int j = 0; j <= m; j++) {
-        //         System.out.format("%4d", prefix[i][j]);
-        //     }
-        //     System.out.println();
-        // }
+        for(int side = Math.min(n, m); side > 0; side--) {
+            for(int i = 0; i + side <= n; i++) {
+                for(int j = 0; j + side <= m; j++) {
+                    int R1 = i;
+                    int C1 = j;
 
-        for(int side = Math.min(n, m) - 1; side > 0; side--) {
-            for(int i = 1; i + side <= n; i++) {
-                for(int j = 1; j + side <= m; j++) {
-                    // System.out.println("prefix[i + side][j + side]: " +prefix[i + side][j + side]
-                    //                   +" prefix[i - 1][j + side]: "+prefix[i - 1][j + side]
-                    //                   +" prefix[i + side][j - 1]: "+prefix[i + side][j - 1]
-                    //                   +" prefix[i - 1][j - 1]: "+prefix[i - 1][j - 1]);
-                    int sum = prefix[i + side][j + side]
-                            - prefix[i - 1][j + side] - prefix[i + side][j - 1]
-                            + prefix[i - 1][j - 1];
-                    if(sum <= threshold) return side + 1;
+                    int R2 = i + side;
+                    int C2 = j + side;
+
+                    int sum = prefix[R2][C2] - prefix[R1][C2] - prefix[R2][C1] + prefix[R1][C1];
+
+                    if(sum <= threshold) return side;
                 }
             }
         }
