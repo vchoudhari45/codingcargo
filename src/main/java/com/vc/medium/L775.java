a73b4cc4fc275497d7da76a0632ea97039df40e7
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-06 23:18:17 +0530

    L775

--- src/main/java/com/vc/medium/L775.java
+++ src/main/java/com/vc/medium/L775.java
@@ -2,12 +2,14 @@ package com.vc.medium;
 
 class L775 {
     public boolean isIdealPermutation(int[] A) {
-        if(A == null && A.length == 0) return true;
+        if(A == null || A.length == 0) return true;
 
-        int N = A.length;
-        for (int i = 0; i < N; ++i)
-            for (int j = i + 2; j < N; ++j)
-                if (A[i] > A[j]) return false;
+        int n = A.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 2; j < n; j++) {
+                if(A[i] > A[j]) return false;
+            }
+        }
         return true;
     }
 }
