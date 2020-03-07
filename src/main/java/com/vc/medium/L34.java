42260f5b2a709b5f58644bf550f803f04960c5fa
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-08 01:28:15 +0530

    L34

--- src/main/java/com/vc/medium/L34.java
+++ src/main/java/com/vc/medium/L34.java
@@ -2,34 +2,17 @@ package com.vc.medium;
 
 class L34 {
     public int[] searchRange(int[] arr, int target) {
-        int lo = findFirst(arr, target);
-        int hi = findLast(arr, target);
-        return new int[] {hi, lo};
-    }
+        if(arr == null || arr.length == 0) return new int[]{-1, -1};
 
-    private int findFirst(int[] arr, int target) {
-        int index = -1;
-        int lo = 0;
-        int hi = arr.length - 1;
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(arr[mid] > target) hi = mid - 1;
-            else lo = mid + 1;
-            if(arr[mid] == target) index = mid;
-        }
-        return index;
-    }
-
-    private int findLast(int[] arr, int target) {
-        int index = -1;
-        int lo = 0;
-        int hi = arr.length - 1;
-        while(lo <= hi) {
+        int lo = 0, hi = arr.length - 1, n = arr.length;
+        while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
             if(arr[mid] < target) lo = mid + 1;
-            else hi = mid - 1;
-            if(arr[mid] == target) index = mid;
+            else hi = mid;
         }
-        return index;
+        if(arr[lo] != target) return new int[]{-1, -1};
+        int index = lo;
+        while(index < n && arr[index] == target) index++;
+        return new int[]{lo, index - 1};
     }
 }
