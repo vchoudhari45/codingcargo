397e29504bec8d7ab3b005a58a96f5d80a1e8fe7
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-07 17:46:17 +0530

    L333

--- src/main/java/com/vc/medium/L333.java
+++ src/main/java/com/vc/medium/L333.java
@@ -3,24 +3,40 @@ package com.vc.medium;
 import com.vc.hard.TreeNode;
 
 class L333 {
-    int max = 0;
+
+    class Entry {
+        int size;
+        long leftValue, rightValue;
+
+        Entry(int size, long leftValue, long rightValue) {
+            this.size = size;
+            this.leftValue = leftValue;
+            this.rightValue = rightValue;
+        }
+    }
+
+    private int max = 0;
     public int largestBSTSubtree(TreeNode root) {
-        solve(root);
+        helper(root);
         return max;
     }
 
-    private int[] solve(TreeNode root) {
-        if(root == null) return new int[]{0, Integer.MAX_VALUE, Integer.MIN_VALUE};
+    private Entry helper(TreeNode root) {
+        if(root == null) return new Entry(0, Long.MAX_VALUE, Long.MIN_VALUE);
+        else {
+            Entry left = helper(root.left);
+            Entry right = helper(root.right);
 
-        int[] left = solve(root.left);
-        int[] right = solve(root.right);
+            if(left.size != -1 && right.size != -1 &&
+                    root.val > left.rightValue && root.val < right.leftValue) {
+                int size = left.size + right.size + 1;
+                max = Math.max(max, size);
+                return new Entry(size,
+                        Math.min(root.val, left.leftValue),
+                        Math.max(root.val, right.rightValue));
+            }
 
-        if(left[0] == -1 || right[0] == -1 || root.val <= left[2] || root.val >= right[1]) {
-            return new int[]{-1, Integer.MAX_VALUE, Integer.MIN_VALUE};
+            return new Entry(-1, Long.MAX_VALUE, Long.MIN_VALUE);
         }
-
-        int size = left[0] + 1 + right[0];
-        max = Math.max(max, size);
-        return new int[]{size, Math.min(left[1], root.val), Math.max(right[2], root.val)};
     }
 }
