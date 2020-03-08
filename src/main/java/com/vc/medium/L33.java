f06e42f9f11dce9809e7772fa5a9d45cdd9cc375
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-08 11:47:27 +0530

    L33

--- src/main/java/com/vc/medium/L33.java
+++ src/main/java/com/vc/medium/L33.java
@@ -1,26 +1,41 @@
 package com.vc.medium;
 
 class L33 {
+
+    private int[] arr;
+    private int target;
+
     public int search(int[] arr, int target) {
-        int lo = 0;
-        int hi = arr.length - 1;
+        this.arr = arr;
+        this.target = target;
+
+        int minIndex = getMinIndex();
+
+        int index = search(0, minIndex - 1);
+        if(index == -1) index = search(minIndex, arr.length - 1);
+
+        return index;
+    }
+
+    private int search(int lo, int hi) {
         while(lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-
-            if(arr[mid] == target) return mid;
-
-            if(arr[lo] == arr[mid] && arr[hi] == arr[mid]) {
-                lo++; hi--;
-            }
-            else if(arr[lo] <= arr[mid]) {
-                if(arr[lo] <= target && arr[mid] > target) hi = mid - 1;
-                else lo = mid + 1;
-            }
-            else {
-                if(arr[mid] < target && arr[hi] >= target) lo = mid + 1;
-                else hi = mid - 1;
-            }
+            if(arr[mid] > target) hi = mid - 1;
+            else if(arr[mid] < target) lo = mid + 1;
+            else return mid;
         }
         return -1;
+    }
+
+    private int getMinIndex() {
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(arr[mid] > arr[hi]) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
     }
 }
