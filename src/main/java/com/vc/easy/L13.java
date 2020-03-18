d592245b38ece45f9d37de71fdfe22998553662a
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-18 12:05:48 -0700

    L13

---
+++ src/main/java/com/vc/easy/L13.java
@@ -0,0 +1,43 @@
+package com.vc.easy;
+
+import java.util.*;
+
+class L13 {
+
+    public int romanToInt(String s) {
+
+        Map<String, Integer> values = new HashMap<>();
+
+        values.put("M", 1000);
+        values.put("D", 500);
+        values.put("C", 100);
+        values.put("L", 50);
+        values.put("X", 10);
+        values.put("V", 5);
+        values.put("I", 1);
+
+        int sum = 0;
+        int i = 0;
+        while (i < s.length()) {
+            String currentSymbol = s.substring(i, i + 1);
+
+            int currentValue = values.get(currentSymbol);
+            int nextValue = 0;
+            if (i + 1 < s.length()) {
+                String nextSymbol = s.substring(i + 1, i + 2);
+                nextValue = values.get(nextSymbol);
+            }
+
+            if (currentValue < nextValue) {
+                sum += (nextValue - currentValue);
+                i += 2;
+            }
+            else {
+                sum += currentValue;
+                i += 1;
+            }
+
+        }
+        return sum;
+    }
+}
