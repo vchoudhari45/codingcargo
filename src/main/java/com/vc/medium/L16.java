647a16e377f0113ce398502df6a1a33dd5ddde9e
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-18 22:02:22 -0700

    L16

--- src/main/java/com/vc/medium/L16.java
+++ src/main/java/com/vc/medium/L16.java
@@ -3,35 +3,24 @@ package com.vc.medium;
 import java.util.Arrays;
 
 class L16 {
-    public int threeSumClosest(int[] arr, int target) {
-        Arrays.sort(arr);
-        //for(Integer i: arr) System.out.print(i+", ");
-        //System.out.println("\nStarting.....");
-        int closest = Integer.MAX_VALUE;
-        int ans = 0;
-        for(int f = 0; f < arr.length - 2; f++) {
-            int s = f + 1;
-            int t = arr.length - 1;
-            int first = arr[f];
-            while(s < t) {
-                int second = arr[s];
-                int third = arr[t];
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
 
-                int sum = first + second + third;
-
-                //System.out.println("first: "+first+" second: "+second+" third: "+third+" sum: "+sum);
-
-                if(sum == target) return sum;
-
-                else if(target - sum > 0) s++;
-                else if(target - sum < 0) t--;
-
-                if(closest > Math.abs(sum - target)) {
-                    closest = Math.abs(sum - target);
-                    ans = sum;
+        Arrays.sort(nums);
+        int n = nums.length, diff = Integer.MAX_VALUE, sum = 0;
+        for(int first = 0; first < n; first++) {
+            int second = first + 1, third = n - 1;
+            while(second < third) {
+                int s = nums[first] + nums[second] + nums[third];
+                if(Math.abs(target - s) < diff) {
+                    diff = Math.abs(target - s);
+                    sum = s;
                 }
+                if(s > target) third--;
+                else if(s < target) second++;
+                else return target;
             }
         }
-        return ans;
+        return sum;
     }
 }
