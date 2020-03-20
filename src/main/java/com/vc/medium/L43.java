729efb243ac5d336ef71e4e60e651425d928daf2
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-20 09:00:16 -0700

    L43

--- src/main/java/com/vc/medium/L43.java
+++ src/main/java/com/vc/medium/L43.java
@@ -3,21 +3,24 @@ package com.vc.medium;
 class L43 {
     public String multiply(String num1, String num2) {
         int m = num1.length(), n = num2.length();
-        int[] pos = new int[m + n];
+        int[] res = new int[m + n];
 
+        int pos = m + n - 1;
         for(int i = m - 1; i >= 0; i--) {
+            int position = pos;
             for(int j = n - 1; j >= 0; j--) {
                 int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int p1 = i + j, p2 = i + j + 1;
-                int sum = mul + pos[p2];
 
-                pos[p1] += sum / 10;
-                pos[p2] = (sum) % 10;
+                int sum = mul + res[position];
+                res[position - 1] += sum / 10;
+                res[position] = (sum) % 10;
+                position--;
             }
+            pos--;
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+        for(int p : res) if(!(sb.length() == 0 && p == 0)) sb.append(p);
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
