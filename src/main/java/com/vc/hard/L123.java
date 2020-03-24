81a772260dda90b701790bc5a09d929e234fe369
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-24 13:36:26 -0700

    L123

--- src/main/java/com/vc/hard/L123.java
+++ src/main/java/com/vc/hard/L123.java
@@ -5,11 +5,69 @@ import java.util.*;
 class L123 {
     public int maxProfit(int[] prices) {
         /**
-             dp[i][k][0]
-             On ith day with K transaction in total we should have zero stock left in our hand
+             Input:
+             [3 3 5 0 0 3 1 4]
+             1 2 3 4 5 6 7 8
 
-             dp[i][k][1]
-             On ith day with K transaction in total we should have one stock left in our hand
+             k = 2
+             Number of transactions allowed is 2, which means 2 Buys and 2 Sells are allowed
+
+             Explanation:
+             dp_i_0[k] -> Profit on ith day after k transaction and zero stock in hand at end of day
+             dp_i_1[k] -> Profit on ith day after k transactions and one stock in hand at end of day
+
+
+             Formula:
+             dp_i_1[j] = Math.max(dp_i_1[j], dp_i_0[j - 1] - prices[i])
+             -> On ith Day we can either do nothing which gives us dp_i_1[j] profit
+             -> Or Perform buy which gives us dp_i_0[j - 1] - prices[i]
+             -> dp_i_0[j - 1] because we buy stock with profit made until previous transaction
+             and previous transaction has to be sell because we can't buy again before we sell
+             -> - prices[i]   because we buy with current days price
+
+
+             dp_i_0[j] = Math.max(dp_i_0[j], dp_i_1[j] + prices[i])
+             -> On ith Day we can either do nothing which gives us dp_i_0[j] profit
+             -> Or Perform sell which gives us dp_i_1[j] + prices[i]
+             -> dp_i_1[j]    because we sell stock which was bought at 1st best price
+             -> + prices[i]  because we get profit after selling stock
+
+             0th Day
+             dp_i_1 [-I, -I, -I]
+             dp_i_0 [0,   0,  0]
+
+             1st Day = 3
+             dp_i_1 [-I, -3, -3]
+             dp_i_0 [ 0,  0,  0]
+
+             2nd Day = 3
+             dp_i_1 [-I, -3, -3]
+             dp_i_0 [ 0,  0,  0]
+
+             3rd Day = 5
+             dp_i_1 [-I, -3, -3]
+             dp_i_0 [ 0,  2,  2]
+
+             4th Day = 0
+             dp_i_1 [-I,  0,  2]
+             dp_i_0 [ 0,  2,  2]
+
+             5th Day = 0
+             dp_i_1 [-I,  0,  2]
+             dp_i_0 [ 0,  2,  2]
+
+             6th Day = 3
+             dp_i_1 [-I,  0,  2]
+             dp_i_0 [ 0,  3,  5]
+
+             7th Day = 1
+             dp_i_1 [-I,  0,  2]
+             dp_i_0 [ 0,  3,  5]
+
+
+             8th Day = 4
+             dp_i_1 [-I,  0,  2]
+             dp_i_0 [ 0,  4,  6]
          */
         int n = prices.length;
         if(n <= 1) return 0;
