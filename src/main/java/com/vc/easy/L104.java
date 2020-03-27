aba5f35c1d64e7c187d70041c579c11b2a070103
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-27 09:43:41 -0700

    L104

---
+++ src/main/java/com/vc/easy/L104.java
@@ -0,0 +1,27 @@
+package com.vc.easy;
+
+import com.vc.hard.TreeNode;
+
+import java.util.*;
+
+class L104 {
+    public int maxDepth(TreeNode root) {
+        //Level order traversal
+        if(root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int level = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size > 0) {
+                TreeNode node = q.poll();
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+                size--;
+            }
+            level++;
+        }
+        return level;
+    }
+}
