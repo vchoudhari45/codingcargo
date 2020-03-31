6fc1db4ce6b1aa0ff438a884f95486b87175b762
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-03-31 00:46:35 -0700

    L226

---
+++ src/main/java/com/vc/easy/L226.java
@@ -0,0 +1,23 @@
+package com.vc.easy;
+
+import com.vc.hard.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class L226 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            if(node.left != null) q.offer(node.left);
+            if(node.right != null) q.offer(node.right);
+        }
+        return root;
+    }
+}
