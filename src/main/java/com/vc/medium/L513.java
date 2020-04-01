62735db8da9321634639c344a768753fa9c7125d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-04-01 11:43:52 -0700

    L513

---
+++ src/main/java/com/vc/medium/L513.java
@@ -0,0 +1,27 @@
+package com.vc.medium;
+
+import com.vc.hard.TreeNode;
+
+import java.util.*;
+
+class L513 {
+    public int findBottomLeftValue(TreeNode root) {
+        if(root == null) return 0;
+
+        int result = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == 0) result = node.val;
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+
+        return result;
+    }
+}
