366b0e66a90b91005327e143358d97258a563694
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-04-02 04:36:26 -0700

    L669

---
+++ src/main/java/com/vc/easy/L669.java
@@ -0,0 +1,15 @@
+package com.vc.easy;
+
+import com.vc.hard.TreeNode;
+
+class L669 {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root == null) return null;
+        if(root.val > R) return trimBST(root.left, L, R);
+        if(root.val < L) return trimBST(root.right, L, R);
+
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
