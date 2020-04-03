3d65fec304f98fa416edde6d85b0436b2e23387b
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-04-03 14:09:31 -0700

    L1059

--- src/main/java/com/vc/medium/L1059.java
+++ src/main/java/com/vc/medium/L1059.java
@@ -3,31 +3,32 @@ package com.vc.medium;
 import java.util.*;
 
 class L1059 {
+    private final static int NOT_STARTED = 0, PROCESSING = 1, PROCESSED = 2;
+
     public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
 
+        int[] states = new int[n];
         for(int[] edge: edges) {
-            int from = edge[0];
-            int to = edge[1];
+            int from = edge[0], to = edge[1];
 
-            List<Integer> list = map.getOrDefault(from, new ArrayList<>());
-            list.add(to);
-            map.put(from, list);
+            List<Integer> fromList = graph.getOrDefault(from, new ArrayList<>());
+            fromList.add(to);
+            graph.put(from, fromList);
         }
 
-        return dfs(source, destination, map, new HashSet<Integer>());
+        return dfs(source, destination, states, graph);
     }
 
-    private boolean dfs(int current, int target, HashMap<Integer, List<Integer>> map,
-                        HashSet<Integer> visited) {
-        if(!map.containsKey(current)) return current == target;
+    private boolean dfs(int current, int destination, int[] states, HashMap<Integer, List<Integer>> graph) {
+        if(states[current] != NOT_STARTED) return states[current] == PROCESSED;
 
-        if(visited.contains(current)) return false;
-
-        visited.add(current);
-        for(int child : map.get(current)) if(!dfs(child, target, map, visited)) return false;
-        visited.remove(current);
-
+        if(!graph.containsKey(current)) return current == destination;
+        states[current] = PROCESSING;
+        for(int child: graph.get(current)) {
+            if(!dfs(child, destination, states, graph)) return false;
+        }
+        states[current] = PROCESSED;
         return true;
     }
 }
