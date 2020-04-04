6c597291bb8a70ac51049907f89e69d6e201b3e0
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-04-04 10:03:45 -0700

    L1153

--- src/main/java/com/vc/hard/L1153.java
+++ src/main/java/com/vc/hard/L1153.java
@@ -6,22 +6,15 @@ class L1153 {
     public boolean canConvert(String str1, String str2) {
         int n1 = str1.length(), n2 = str2.length();
         if(n1 != n2) return false;
-        if(str1.equals(str2)) return true;
 
-        /**
-            "abcdefghijklmnopqrstuvwxyz"
-            "bcdefghijklmnopqrstuvwxyza"
-         */
         HashSet<Character> set = new HashSet<>();
-        for(int i = 0; i < n1; i++) set.add(str2.charAt(i));
-        if(set.size() >= 26) return false;
-
         HashMap<Character, Character> map = new HashMap<>();
         for(int i = 0; i < n1; i++) {
             char ch1 = str1.charAt(i), ch2 = str2.charAt(i);
+            set.add(ch2);
             if(map.containsKey(ch1) && map.get(ch1) != ch2) return false;
             map.put(ch1, ch2);
         }
-        return true;
+        return set.size() < 26 || str1.equals(str2);
     }
 }
