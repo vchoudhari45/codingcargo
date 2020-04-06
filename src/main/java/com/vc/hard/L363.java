afa6db82cf859150f770d000bdd71f08a3425a52
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-04-06 14:05:26 -0700

    L368

--- src/main/java/com/vc/hard/L363.java
+++ src/main/java/com/vc/hard/L363.java
@@ -4,41 +4,31 @@ import java.util.*;
 
 class L363 {
     public int maxSumSubmatrix(int[][] matrix, int k) {
-        /**
-             [
-                [1, 0,1],
-                [0,-2,3]
-             ]
-         */
-        int rn = matrix.length;
-        if(rn == 0) return 0;
-        int cn = matrix[0].length;
-        int max = Integer.MIN_VALUE;
-        for(int c1 = 0; c1 < cn; c1++) {
-            int[] c = new int[rn];
-            for(int c2 = c1; c2 < cn; c2++) {
-                for(int r = 0; r < rn; r++) {
-                    c[r] += matrix[r][c2];
+        if(matrix == null || matrix.length == 0) return 0;
+
+        int n = matrix.length, m = matrix[0].length, max = Integer.MIN_VALUE;
+        for(int fromRow = 0; fromRow < n; fromRow++) {
+            int[] colSum = new int[m];
+            for(int toRow = fromRow; toRow < n; toRow++) {
+                for(int col = 0; col < m; col++) {
+                    colSum[col] += matrix[toRow][col];
                 }
-                max = Math.max(max, maxArray(c, k));
+                max = Math.max(max, getSum(colSum, k));
             }
         }
         return max;
     }
 
-    private int maxArray(int[] arr, int k) {
-        //System.out.println("\nArray: ");
-        //for(int i = 0; i < arr.length; i++) System.out.print(arr[i]+" ");
-
-        TreeSet<Integer> sum = new TreeSet<>();
-        sum.add(0);
-        int max = Integer.MIN_VALUE;
-        int curSum = 0;
+    private int getSum(int[] arr, int k) {
+        //System.out.println(Arrays.toString(arr));
+        TreeSet<Integer> set = new TreeSet();
+        set.add(0);
+        int currentSum = 0, max = Integer.MIN_VALUE;
         for(int i = 0; i < arr.length; i++) {
-            curSum += arr[i];
-            Integer num = sum.ceiling(curSum - k);
-            if(num != null) max = Math.max(max, curSum - num);
-            sum.add(curSum);
+            currentSum += arr[i];
+            Integer num = set.ceiling(currentSum - k);
+            if(num != null) max = Math.max(max, currentSum - num);
+            set.add(currentSum);
         }
         return max;
     }
--- src/main/java/com/vc/medium/L368.java
+++ src/main/java/com/vc/medium/L368.java
@@ -3,37 +3,37 @@ package com.vc.medium;
 import java.util.*;
 
 class L368 {
-    public List<Integer> largestDivisibleSubset(int[] arr) {
-        int n = arr.length;
-        if(n == 0) return new ArrayList<Integer>();
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        if(nums == null || nums.length == 0) return new ArrayList<Integer>();
 
-        int[] dp = new int[n];
-        Arrays.sort(arr);
-        for(int i = 1; i < n; i++) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        List<Integer>[] dp = new List[n];
+        for(int i = 0; i < n; i++) dp[i] = new ArrayList<>();
+
+        //Longest increasing Subsequence like
+        for(int i = 0; i < n; i++) {
+            List<Integer> maxSet = new ArrayList<>();
+
             for(int j = 0; j < i; j++) {
-                if(arr[i] % arr[j] == 0)
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if(nums[i] % nums[j] == 0 && maxSet.size() < dp[j].size()) {
+                    maxSet = dp[j];
+                }
             }
-        }
 
-        int maxIndex = 0;
-        int max = 0;
-        for(int i = 0; i < dp.length; i++) {
-            if(max < dp[i]) {
-                max = dp[i];
-                maxIndex = i;
-            }
+            dp[i].addAll(maxSet);
+            dp[i].add(nums[i]);
+            //System.out.println(Arrays.toString(dp));
         }
 
         List<Integer> res = new ArrayList<>();
-        res.add(arr[maxIndex]);
-        for(int i = maxIndex; i >= 0; i--){
-            if(dp[i] + 1 == dp[maxIndex]) {
-                maxIndex = i;
-                res.add(arr[maxIndex]);
+        for(int i = 0; i < n; i++) {
+            if(dp[i].size() > res.size()) {
+                res = dp[i];
             }
         }
-
         return res;
     }
 }
