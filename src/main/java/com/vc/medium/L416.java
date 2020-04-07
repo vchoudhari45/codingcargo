aada88086a7f41816598105d730bd1beddb17d02
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-04-07 08:47:14 -0700

    L416

--- src/main/java/com/vc/medium/L416.java
+++ src/main/java/com/vc/medium/L416.java
@@ -2,21 +2,38 @@ package com.vc.medium;
 
 class L416 {
     public boolean canPartition(int[] arr) {
+        int n = arr.length;
+
         int sum = 0;
         for(int i = 0; i < arr.length; i++) sum += arr[i];
-        if(sum % 2 != 0) return false;
 
+        if(sum % 2 != 0) return false;
         sum /= 2;
 
-        boolean[] dp = new boolean[sum+1];
-        dp[0] = true;
-        for (int num : arr) {
-            for (int i = sum; i > 0; i--) {
-                if (i >= num) {
-                    dp[i] = dp[i] || dp[i-num];
+        boolean[][] dp = new boolean[arr.length + 1][sum + 1];
+
+        /**
+               0 1 2 3 4
+             0 T F F F F
+             1 T T F F F
+             2 T T T T F
+             1 T T T T T
+         */
+        //if this is zero sum it is possible to make it with any subset
+        for(int i = 0; i < dp.length; i++) dp[i][0] = true;
+
+        for(int i = 1; i < dp.length; i++) {
+            int num = arr[i - 1];
+            for(int sumValue = 1; sumValue < dp[0].length; sumValue++) {
+                if(num > sumValue) {
+                    dp[i][sumValue] = dp[i - 1][sumValue];
+                }
+                else {
+                    int remainingSum = sumValue - num;
+                    dp[i][sumValue] = dp[i - 1][sumValue] || dp[i - 1][remainingSum];
                 }
             }
         }
-        return dp[sum];
+        return dp[arr.length][sum];
     }
 }
