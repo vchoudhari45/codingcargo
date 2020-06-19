701bf47934fd606623097f65d364d68563c7a8db
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-06-19 16:21:09 +0530

    NprogressBar

--- src/main/java/com/vc/web/ui/src/component/defaultLayout.tsx
+++ src/main/java/com/vc/web/ui/src/component/defaultLayout.tsx
@@ -1,27 +1,7 @@
 import Header from './layout/header'
 import Footer from './layout/footer'
-import { useRouter } from 'next/dist/client/router';
-import { useState, useEffect } from 'react';
 
 const DefaultLayout: React.FC = ({ children }) => {
-    const router = useRouter();
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        const handleStart = (url: string) => (url !== router.pathname) && setLoading(true);
-        const handleComplete = (url: string) => (url !== router.pathname) && setLoading(false);
-
-        router.events.on('routeChangeStart', handleStart)
-        router.events.on('routeChangeComplete', handleComplete)
-        router.events.on('routeChangeError', handleComplete)
-
-        return () => {
-            router.events.off('routeChangeStart', handleStart)
-            router.events.off('routeChangeComplete', handleComplete)
-            router.events.off('routeChangeError', handleComplete)
-        }
-    })
-
     return (
         <>
             <Header />
@@ -30,9 +10,7 @@ const DefaultLayout: React.FC = ({ children }) => {
                                         pt-3    pr-6    pb-3    pl-8
                                         md:pt-3 md:pr-10 md:pb-3 md:pl-12
                                         lg:pt-3 lg:pr-20 lg:pb-3 lg:pl-20">
-                        {
-                            loading ? (<div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20"></div>) : children
-                        }
+                        {children}
                     </div>
                 </main>
             <Footer />
--- src/main/java/com/vc/web/ui/src/pages/_app.tsx
+++ src/main/java/com/vc/web/ui/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from 'next/app'
 import { defaultLayout } from '../component/defaultLayout'
+import { Router } from 'next/dist/client/router'
+import NProgress from 'nprogress'
 import '../css/index.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start()) 
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = ({ Component, pageProps, router }: AppProps) => {
     return defaultLayout(<Component {...pageProps} />)
