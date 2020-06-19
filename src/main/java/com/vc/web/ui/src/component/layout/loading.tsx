73fdf4aeebf7586761f97210d3f45807a80c9f69
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-06-19 15:17:38 +0530

    Adding loading component

--- src/main/java/com/vc/web/ui/src/component/layout/loading.tsx
+++ src/main/java/com/vc/web/ui/src/component/layout/loading.tsx
@@ -1,7 +1,24 @@
+import { useRouter } from "next/dist/client/router";
+import { useState, useEffect } from "react";
+
 export const Loading = () => {
-    return (
-        <>
-            loading....
-        </>
-    )
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const handleStart = (url: string) => (url !== router.pathname) && setLoading(true);
+        const handleComplete = (url: string) => (url !== router.pathname) && setLoading(false);
+
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleComplete)
+        router.events.on('routeChangeError', handleComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleComplete)
+            router.events.off('routeChangeError', handleComplete)
+        }
+    })
+
+    return loading && (<div>Loading....</div>);
 }
