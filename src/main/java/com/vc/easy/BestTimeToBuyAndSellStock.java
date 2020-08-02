87a7029fc80da04f3be92dcbc946e1123e38bc8b
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-08-02 15:19:35 +0530

    Added explanation

--- src/main/java/com/vc/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/vc/easy/BestTimeToBuyAndSellStock.java
@@ -7,16 +7,18 @@ package com.vc.easy;
  *  Title: Best Time to Buy and Sell Stock
  *  MetaDescription: Java Solution to Data Structures And Algorithms problem, Say you have an array for which the ith element is the price of a given stock on day i. If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit. Note that you cannot sell a stock before you buy one, It is an easy problem that uses Dynamic Programming to solve the problem.
  *  Description: Say you have an array for which the ith element is the price of a given stock on day i. If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ *  Input: [7,1,5,3,6,4]
+ *  Output: 5
  *  Similar Question: Best Time to Buy and Sell Stock II, Best Time to Buy and Sell Stock III, Best Time to Buy and Sell Stock IV, Best Time to Buy and Sell Stock with cool down.
  *****/
 
 class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
         /**
-             dp[i][k][0]
+             dp[i][k][0] Meaning
              Profit on ith Day, with k transaction in total we should have 0 stocks in our hand
 
-             dp[i][k][1]
+             dp[i][k][1] Meaning
              Profit on ith Day, with k transaction in total we should have 1 stock in our hand
 
              only Buy action reduces number of transaction k
