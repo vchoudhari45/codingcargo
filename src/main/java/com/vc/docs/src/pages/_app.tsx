6f27c505d1aebd37199f12b2874e00ab89fd8c90
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-08-25 09:16:34 +0530

    nProgress

--- src/main/java/com/vc/docs/src/pages/_app.tsx
+++ src/main/java/com/vc/docs/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from 'next/app'
 import DefaultLayout from '../component/defaultLayout'
 import '../css/index.css'
+import { Router } from 'next/dist/client/router'
+import NProgress from 'nprogress'
+
+NProgress.configure({ showSpinner: false, easing: 'ease', speed: 1});
+Router.events.on('routeChangeStart', () => NProgress.start()) 
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = (appProps: AppProps) => {
 	return <DefaultLayout {...appProps.pageProps} />
