efda787cd071a389811372350dcb440d718f2b72
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-09-06 23:07:56 +0530

    Fixing favicon

--- src/main/java/com/vc/docs/src/pages/_document.tsx
+++ src/main/java/com/vc/docs/src/pages/_document.tsx
@@ -22,7 +22,34 @@ class CustomDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+				  <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QA/wD/AP+gvaeTAAAA
+B3RJTUUH5AgfCzAQt682SgAABRdJREFUaN7t2m1snWUZB/Df/ZxtnbCOhi0UNsCZKhqTEgKMN40g
+GghESMQwRVRARMJLePtA0GEIH4aGwDIYytwABSYwRE0gJnwxajAEorCEYQHBWQIUGFBgzdhG23Pz
+4XoO3Yd2fc4p21kT/0nTc5pz38//uu/r5X9dp8kOWLCoF2bhc1iMQ7AQnXY/Mt7DAP6Dp/ASPhzo
+3/Dxh1KDeJJTlhbjYpyI/TGzDcTHw4d4A3/FKtmTkjzQv0FasKjXtqHtZnd2nInlOLDdbCfB67gm
+57w2JfVaZ1e3GR0zjsIdOKjd7CqgE19KKa0nbSyEm1yEg9vNrAnshyswt8Bn8dV2M2oBx+GIAoei
+u91sWkAXvlzgC5jdbjYtIKG3wKfbzWQKOKDAPu1mMQXMKZTFbJpiVtFuBlNEmu4GmDHF9aPCBase
+xDAGUS/fdwnxWGuVwFRu4E3chk1NrPmzEIpfwffRj1V4cXcb8G+ci19ipOKat4VY7MvSSzhKyJeH
+8G38XUjoXWpAvTzFJXi0yQfej8chyZ/H+cZcZz3Oxl1COu8SAz7ACnHyfaJ6n4Z5FdZuFK4ymnKR
+8EP0CP8/XcTCa7gcS/HOJ23AgFB/PxWusD9uwjJ8apK1GWtyyn2QU/2w8rQbz79MSPlDsCXJy3Ee
+nvukDPgXzk7JHXW243DcKzq3ycgTreA9KSci610o2tQGavgW1uGksq4+IuLiUWMZq2kDRvAgvoO/
+5axWhO+vw9dVq+DbRaYaKN8fW5IdD4fh3syl5cFswDlYia3NGrAZP8eP8V+hl5ZitegfquIv+FP5
+erZonObv5PP74UYRawtEir4GV4meuLIBv8D1eL8kvBrXak74vY9by8MgmqZvVFjXgQtwHxbXcrGN
+YhWuxLYqBozihfL3Cfi9cJ1mq/YfxBSB6GMvUX08k3A8HhxN9bNyhMELVQ3YEfPt/MonwquiyDVy
++qn4Wgv7dKE7pYnjuJjgbwdn2ZYZgw/hTDymuaL12yyvL1/PExmr2a6vD+eQV8oFMTGZVcWAhKVJ
+umrvkX33whM4C2uMc4Xj4FncmaSGwWeIBrwqRvFwHFx6mJTK56/AXlUMINzmBmPZ4DVRyK42QTYo
+MYJfCZGmXHuh6vEzJLLQeXEDeR/8TFTxz4y3YGcx0IEf4Xc4sl5LW3NyG74nitN4+IeoE8oy8V1R
++KpgY2nsdUJy9+DXovrPnWjRZEGcRCZ6oBjNS1JWE7l9iRBnwzt8dgtuKR+O3CME22QFLzf2LHJx
+f05GRAZ6QFTjnd5eVS3UI2rBT8rTaJzWMmNx8Ygo/WXMOVeMbCYjf6fQRk/VU70jZecrb70KsWbU
+aMMfbxf+OFQ+aFAIvJUNY1JdL35QYc+tomC9KeJumbjFhRXWovniNFP4dY8o75vKU1wrshUhzi5Q
+fdZaF0r0ZlEvmupRWu2Jjy5Pbi1ewbrMHEgRtEua2OtknCLEXNNICxb1/hHfbNGQYeFKL4ubyMIV
+qk776uUeHS0+/7mpTiVmYt/ypxUUUyD/8QbTGv83oM0YLkQFna7YUgjtPl3xVoHnNTlM2oPQV+AZ
+IQWmGzbjscYNPNluNi3gaTxRCEG1Gu+2m1ETGBI992Cts6sb/ieGUMeaYmXcTeRvyPwmUa8NvbdJ
+Z1d3FiPE58Xcc76QCXvK92e5JP64GHTdnRge6N8wRnDhokPV1SVpHo4Rt/FF8c8fc9pEerMQin1C
+rv8z8W5d9nr/s+AjqkBHsYthbBsAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjAtMDgtMzFUMTE6NDg6
+MTYtMDQ6MDDtGfsJAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIwLTA4LTMxVDExOjQ4OjE2LTA0OjAw
+nERDtQAAAABJRU5ErkJggg==" rel="icon" type="image/x-icon" />
 					<link rel="manifest" href={BASEURL + "manifest.json"} />
 					<link rel="apple-touch-icon" href={BASEURL + "apple-touch-icon.png"} />
 
