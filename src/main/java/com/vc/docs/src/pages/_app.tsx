79b7db4a3fbd8c1f9794d08810ed043edc78c69d
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-10-28 20:00:36 +0530

    Adding tracking info

--- src/main/java/com/vc/docs/src/pages/_app.tsx
+++ src/main/java/com/vc/docs/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import ReactGA from 'react-ga'
 import Head from 'next/head'
 import { HOMEPAGE } from '../data/menu'
 
-ReactGA.initialize('G-10ENJJL95K', {titleCase: false, debug: false})
+ReactGA.initialize('G-DTNBVNQE38', {titleCase: false, debug: false})
 const App = (appProps: AppProps) => {
 	const title = appProps.pageProps.selected ? appProps.pageProps.selected.title : HOMEPAGE
 	return (
