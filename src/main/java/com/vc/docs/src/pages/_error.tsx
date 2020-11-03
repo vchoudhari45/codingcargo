f58f85a9931ca04c825c240f4a022fbdbfe4d104
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-11-03 22:49:49 +0530

    Fixing error page

--- src/main/java/com/vc/docs/src/pages/_error.tsx
+++ src/main/java/com/vc/docs/src/pages/_error.tsx
@@ -3,7 +3,7 @@ function Error({ statusCode }) {}
 Error.getInitialProps = ({ res, err }) => {
 	const statusCode = res && res.statusCode ? res.statusCode : err && err.statusCode ? err.statusCode : 404
 	const headerMessage = statusCode == 404 ? "<h1>Page not found</h1>" : "<h1>Something went wrong</h1>"
-	const contentMessage = statusCode == 404 ? "<p>We apologize for the inconvenience, we can’t find the page you were looking for.</p><p><a href='https://www.codingcargo.com/' class='absolute'>Go to homepage</a></p>" : "<p>We apologize for the inconvenience, Something is broken.</p><p><a href='https://www.codingcargo.com/' class='absolute'>Go to homepage</a></p>"
+	const contentMessage = statusCode == 404 ? "<p>We apologize for the inconvenience, but we can’t find the page you were looking for.</p><p><a href='https://www.codingcargo.com/' class='absolute'>Go to homepage</a></p>" : "<p>We apologize for the inconvenience, but we are unable to process your request at this time. Our engineers have been notified of this problem and will work to resolve it.</p><p><a href='https://www.codingcargo.com/' class='absolute'>Go to homepage</a></p>"
 	return { 
 		content: "[comment]: metadata=Coding Cargo Error page\n[comment]: keywords=Coding Cargo, Error Page\n[comment]: robots=noindex, nofollow\n"+headerMessage+"\n"+contentMessage,
 		selected: { title: "Coding Cargo error page" }
