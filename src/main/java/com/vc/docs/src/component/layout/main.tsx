3891c1be6cfaae3f45b1c9bf75f911bcb690a124
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-11-04 01:47:52 +0530

    Fixing ads

--- src/main/java/com/vc/docs/src/component/layout/main.tsx
+++ src/main/java/com/vc/docs/src/component/layout/main.tsx
@@ -102,9 +102,9 @@ const main: React.FC<Props> = ({prev, current, next, content}: Props) => {
 					: ""
 				}
 
-				<div className="ad">
+				{/* <div>
 				  <GoogleAd />
-				</div>
+				</div> */}
 
 			</div>
 		</>
