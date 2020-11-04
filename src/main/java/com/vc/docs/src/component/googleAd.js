5655108adb8fc0b2e065df27f3938963ba89f1be
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-11-04 10:57:52 +0530

    Fixing width

--- src/main/java/com/vc/docs/src/component/googleAd.js
+++ src/main/java/com/vc/docs/src/component/googleAd.js
@@ -10,7 +10,7 @@ const GoogleAd = () => {
              data-ad-client="ca-pub-5837843548871883"
              data-ad-slot="1405717915"
              data-ad-format="auto"
-             inline-style="display:inline-block;width:100%;"
+             inline-style="width:100%;"
              data-full-width-responsive="true"></ins>
   );
 };
