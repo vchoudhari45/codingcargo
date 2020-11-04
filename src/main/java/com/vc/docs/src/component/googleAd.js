388fe36791051ee036c4c6d29b4b345869670597
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-11-04 10:46:31 +0530

    Adding googleAds

--- src/main/java/com/vc/docs/src/component/googleAd.js
+++ src/main/java/com/vc/docs/src/component/googleAd.js
@@ -6,7 +6,7 @@ const GoogleAd = () => {
   }, []);
 
   return (
-		<ins class="adsbygoogle ad_responsive"
+		<ins class="adsbygoogle"
              data-ad-client="ca-pub-5837843548871883"
              data-ad-slot="1405717915"
              data-ad-format="auto"
