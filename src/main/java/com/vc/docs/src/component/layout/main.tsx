53b107f7348517a9edd662d01e377c5155507e2c
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-11-04 10:39:02 +0530

    Adding google adUnit

--- src/main/java/com/vc/docs/src/component/layout/main.tsx
+++ src/main/java/com/vc/docs/src/component/layout/main.tsx
@@ -102,7 +102,7 @@ const main: React.FC<Props> = ({prev, current, next, content}: Props) => {
 					: ""
 				}
 
-				<div>
+				<div className="ad_responsive">
 				This is Google AdUnit
 {/* 				  <GoogleAd /> */}
 				</div>
