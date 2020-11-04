f8bf24b8eff4f38feba135693c3fc9e02883a36b
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-11-04 12:24:32 +0530

    Adding media query

--- src/main/java/com/vc/docs/src/component/googleAd.js
+++ src/main/java/com/vc/docs/src/component/googleAd.js
@@ -6,7 +6,8 @@ const GoogleAd = () => {
   }, []);
 
   return (
-		<ins class="adsbygoogl ad_slot"
+		<ins class="adsbygoogle"
+				 styles="display:block;min-width:100px;max-width:728px;width:100%;height:90px"
 		     data-ad-client="ca-pub-5837843548871883"
          data-ad-slot="1405717915"></ins>
   );
