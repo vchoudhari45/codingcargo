37b320561764a24e53ecd74f4d39635a4b43e8c8
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2020-11-04 11:22:49 +0530

    Adding inblock size

--- src/main/java/com/vc/docs/src/component/googleAd.js
+++ src/main/java/com/vc/docs/src/component/googleAd.js
@@ -10,6 +10,7 @@ const GoogleAd = () => {
              data-ad-client="ca-pub-5837843548871883"
              data-ad-slot="1405717915"
              data-ad-format="horizontal"
+             style="display:inline-block;min-width:150px;max-width:728px;width: 100%;height:90px"
              data-full-width-responsive="true"></ins>
   );
 };
